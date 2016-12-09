6fc5e0488d737426c27bb7d8c706c89b1f5225c6
Karl Ove Hufthammer <karl.ove.hufthammer@helse-bergen.no>
2016-12-09 09:24:51 +0000

    Optimert litt på korleis uthenting av tilfeldige verdiar vert gjort. Skal no gå ein del raskare, men er samtidig like generell (dvs. ein kan framleis spesialtilpassa kor mange verdiar som skal hentast ut for kvar pasient, og det treng heller ikkje vera like mange for kvar pasient)
    SVN-revisjon: 1099

--- soreg/hente_tilfeldig_data_for_validering.R
+++ soreg/hente_tilfeldig_data_for_validering.R
@@ -1,16 +1,8 @@
-
 # Skript for å hente ut tilfeldige celler fra datasett til validering.
 
-library(dplyr)
-library(magrittr)
-library(rapwhale)
-library(tidyr)
+# Nødvendige pakkar
+library(tidyverse)
 library(haven)
-
-# fixme! disse pakkene skal være en del av avhengighetene til rapwhale
-library(lubridate)
-library(readr)
-
 
 # Mappe på kvalitetstenaren
 # Husk at mappen er slik at man har valgt directory fra source file location
@@ -32,7 +24,6 @@ filnamn = "SoReg_2_Datadump_arsrapport.csv"
 adresse = paste0(mappe, "\\", filnamn)
 
 # Les inn data
-
 d_full = read_delim(
   adresse,
   delim = ";",
@@ -69,11 +60,12 @@ d_full = read_delim(
     EttAarBMI = col_double()
   )
 )
+
 # Fjern utrekna variablar og RESH-ID i oppfølgingar
 d = d_full %>%
   select(-contains("BMI"), -contains("_RESH"))
 
-# Hent ut indeksvariabler for seg, og alle variabler som potensielt kan bli utvalgt for seg.
+# Oversikt over namn på indeksvariablar og datavariablar
 ind_vars = c(
   "PasientID", "Fodselsdato", "PasientAlder", "PasientKjonn",
   "OpererendeRESH", "OperererendeSykehus", "ForlopsID", "OperasjonsID"
@@ -82,47 +74,53 @@ data_vars = names(d) %>%
   setdiff(ind_vars)
 
 # Definer hvor mange variabler som skal hentes for hver pasient.
-# Deretter lag to datasett hvor hele datadumpen er filtrert på indeksvariabler i det ene og
-# alle andre variabler i det andre.
 nvars = 3
-d_ind = d[, ind_vars]
-d_data = d[, data_vars]
 
-# Standard, tom dataramme for innfylling
-nulltab = tibble(
-  varnamn = character(),
-  reg_tal = numeric(), epj_tal = numeric(),
-  reg_dato = numeric(), epj_dato = numeric()
-)
-class(nulltab$reg_dato) = "Date"
-class(nulltab$epj_dato) = "Date"
+# Plukk ut tilfeldige datakolonnar for kvar rad og lagra
+# namnet på kolonnane i ein eigen variabel.
+#
+# Dette kan gjerast på mange måtar (slå deg laus). Det viktige er
+# berre at me får éi rad for kvar tilfeldig valde celle, rada inneheld
+# alle tilhøyrande indeks- og datavariablar og at namnet på
+# cella vert lagra i kolonnen «varnamn».
+res = by_row(d, function(x) {
+  sample(data_vars, nvars)
+}, .collate = "rows", .to = "varnamn")
 
-# Hente tilfeldige verdier per pasient, for n antall variabler
+# Legg til aktuelle resultatkolonnar,
+# med rett variabelklasse (tal, dato &c.)
+res = res %>%
+  mutate(
+    reg_tal = NA_real_, epj_tal = NA_real_,
+    reg_dato = NA_real_, epj_dato = NA_real_
+  )
+class(res$reg_dato) = "Date"
+class(res$epj_dato) = "Date"
 
-res_liste = list()
-for (i in 1:nrow(d_ind)) {
-  aktvars = sample(data_vars, 3) # 3 er antall variabler
-  innfylling = nulltab # den tomme tabellen som ble laget ovenfor skal fylles inn i
-  for (j in seq_along(aktvars)) {
-    aktvar = aktvars[j]
-
-    # Hvor verdier skal fylles inn avhenger av klassen på variabelen
-    reskol = switch(class(d_data[[aktvar]]),
-      integer = "reg_tal",
-      numeric = "reg_tal",
-      Date = "reg_dato",
-      stop("Ugyldig dataformat!")
-    )
-    innfylling[j, "varnamn"] = aktvar
-    innfylling[j, reskol] = d_data[i, aktvar]
-  }
-  # De individuelle tibbles som kommer for hver i skal settes sammen til ett datasett
-  res_liste[[length(res_liste) + 1]] =
-    rep(list(d_ind[i, ]), length(aktvars)) %>%
-    bind_rows() %>%
-    bind_cols(innfylling)
+# Legg til info om kva kolonne resultatet skal lagrast i
+vartypar = sapply(res[data_vars], class)
+if (!all(vartypar %in% c("integer", "numeric", "Date"))) {
+  stop("Finst variabel som ikkje er verken tal eller dato.")
 }
-res = bind_rows(res_liste)
+vartypar = vartypar %>%
+  recode(integer = "reg_tal", numeric = "reg_tal", Date = "reg_dato")
+res$res_kol = vartypar[match(res$varnamn, names(res[data_vars]))]
+
+# Funksjon for å flytta dataverdiane til rett kolonne
+# (er laga for å funka på datarammer med éin unik verdi for res_kol)
+flytt_resultat = function(df) {
+  df[[df$res_kol[1]]] = df[[df$varnamn[1]]]
+  df
+}
+
+# For kvar variabel, flytt verdiane til rett kolonne
+res = res %>%
+  group_by(varnamn) %>%
+  do(flytt_resultat(.))
+
+# Fjern dei gamle datakolonnane
+res = res[!(names(res) %in% data_vars)] %>%
+  select(-.row, -res_kol)
 
 # Resultatene skal ryddes slik at det er tilfeldig rekkefølge på pasientene,
 # men rader med samme pasientnummer skal komme etter hverandre, og variablene skal
