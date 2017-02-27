8d89b247c1e5768736b3fec04fb06995b05bf8f3
Karl Ove Hufthammer <karl.ove.hufthammer@helse-bergen.no>
2017-02-27 16:13:06 +0000

    Lagt til alternative (og meir kompakte) visingar av testresultata
    SVN-revisjon: 1330

--- felles/r-kode/kodebok-fyll.R
+++ felles/r-kode/kodebok-fyll.R
@@ -12,4 +12,7 @@ kb_fyll = function(df, kb, ..., .suffiks = "_tekst") {
 }
 
 # Test at funksjonen fungerer
-test_file("h:/kvalreg/felles/r-kode/kodebok-fyll-testar.R")
+test_adr = "h:/kvalreg/felles/r-kode/kodebok-fyll-testar.R"
+test_file(test_adr, reporter = "minimal") # *Veldig* kort og konsist samandrag
+test_file(test_adr, reporter = "check") # 13-linjes samandrag
+test_file(test_adr, reporter = "summary") # Alt (tar stor plass viss det er mange mislykka testar)
