6772cd8f99f051d724744f8040afc3053d180cbc
Lena Kristin Bache-Mathiesen <lenakba@users.noreply.github.com>
2017-03-01 07:34:44 +0000

    Startet på kb_fyll funksjon
    SVN-revisjon: 1332

--- felles/r-kode/kodebok-fyll.R
+++ felles/r-kode/kodebok-fyll.R
@@ -2,14 +2,59 @@
 #
 # Sjå fila kodebok-fyll-kravspek.pdf for detaljert kravspek for implementasjon
 
+
+# Ikke gjjør om streng-variabler til faktorer automatisk
+options(stringsAsFactors = FALSE)
+
+
 # Nødvendige pakkar
 library(tidyverse)
 library(testthat)
+library(dplyr)
+library(magrittr)
+
+
+# Eksempel:
+
+
+# Eksempeldatasett
+d = tribble(
+  ~pasid, ~kjonn, ~alder, ~med, ~prem,
+  101, 2, 18, 3, 2,
+  102, 1, 37, 4, 2,
+  103, 1, 17, 1, 3
+)
+
+# Eksempelkodebok
+kb = tribble(
+  ~var_id, ~verdi, ~verditekst,
+  "kjonn", 1, "mann",
+  "kjonn", 2, "kvinne",
+  "med", 1, "Antibac",
+  "med", 2, "Insulin",
+  "med", 3, "Ibux",
+  "med", 4, "Globoid",
+  "gensp", 1, "misfornogd",
+  "gensp", 2, "både og",
+  "gensp", 3, "fornøgd"
+)
+
 
 # Definisjon av funksjon
 kb_fyll = function(df, kb, ..., .suffiks = "_tekst") {
-  df # Byt ut med *magi* ... :)
+  if (class(df[[, 1]]) == "character") {
+    vars = colnames(df)
+
+
+    df = df %>%
+      mutate(var_tekst = factor(vars,
+        levels = kb$verdi,
+        labels = kb$verditekst
+      ))
+  }
 }
+
+
 
 # Test at funksjonen fungerer
 test_adr = "h:/kvalreg/felles/r-kode/kodebok-fyll-testar.R"
