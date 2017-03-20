7077899fd422024957307dc61a7c1ef1976f0392
Karl Ove Hufthammer <karl.ove.hufthammer@helse-bergen.no>
2017-03-20 13:26:29 +0000

    Bruk (våre) standardiserte kodebok-kolonnenamn
    SVN-revisjon: 1410

--- felles/r-kode/kodebok-mrs.R
+++ felles/r-kode/kodebok-mrs.R
@@ -9,6 +9,7 @@ options(stringsAsFactors = FALSE)
 # Nødvendige pakkar
 library(readxl) # Lesing av Excel-filer
 library(dplyr) # Datamassering
+library(tibble) # Fornuftig datarammestruktur
 library(stringr) # Tekstmassering
 library(magrittr) # Funksjonar som kan brukast med røyr-operatoren
 
@@ -19,16 +20,12 @@ library(magrittr) # Funksjonar som kan brukast med røyr-operatoren
 # Les inn eksempeldata
 mappe = "***FJERNA-ADRESSE***"
 filnamn = "20160801_Kodebok_NorArtritt.xlsx"
-
-
-
-# mappe = "***FJERNA-ADRESSE***"
-# filnamn = "20160801_Kodebok_NorArtritt.xlsx"
-
 adresse = paste0(mappe, filnamn)
 
 # Les inn (ei fane i) Excel-kodeboka
-d = read_excel(adresse, sheet = 8)
+kb_mrs = read_excel(adresse, sheet = 8)
+
+
 
 # Lag ny kodebok ----------------------------------------------------------
 
@@ -43,14 +40,31 @@ nvars = length(ind_nyvar) # Talet på variablar
 var_nverd = diff(ind_nyvar) - 1
 var_nverd[nvars] = nrow(d) - ind_nyvar[nvars]
 
+# Oversikt over variabeltypar i MRS og standardnamna me brukar
+vartype_mrs_standard = tribble(
+  ~type_mrs, ~type_standard,
+  "Enum", "kategorisk",
+  "Text", "tekst",
+  "Avkrysning", "boolsk",
+  "Dato/Tid", "dato_kl",
+  "Id (Guid)", "tekst",
+  "Numerisk (heltall)", "numerisk",
+  "Numerisk (flyttall)", "numerisk"
+)
+nye_vartypar = na.omit(setdiff(d$Felttype, vartype_mrs_standard$type_mrs))
+if (length(nye_vartypar) > 0) {
+  stop("Kodeboka har variabeltypar me ikkje har standardnamn på: ", str_c(nye_vartypar, collapse = ", "))
+}
+
 # Lag dataramme med i utgangspunktet éi rad for kvar variabel
-kodebok_utg = data_frame(
+kodebok_utg = tibble(
   dd_id = d$DataDumpnavn[ind_nyvar], # Datadumpnamn (teit)
-  var_id = d$Variabelnavn[ind_nyvar], # Variabel-ID (OK)
-  var_type = d$Felttype[ind_nyvar],
+  variabel_id = d$Variabelnavn[ind_nyvar], # Variabel-ID (OK)
+  variabeltype = vartype_mrs_standard$type_standard[match(d$Felttype[ind_nyvar], vartype_mrs_standard$type_mrs)],
+  obligatorisk = str_to_lower(d$Obligatorisk[ind_nyvar]),
   # skjema = d$Skjema[ind_nyvar], #skjematype
-  kode = NA_integer_,
-  kode_tekst = NA_character_,
+  verdi = NA_integer_, # Føreset førbels at MRS-kodane alltid er tal (gjer om til tekst om dette ikkje stemmer)
+  verdi_tekst = NA_character_,
   forklaring = d$Feltnavn[ind_nyvar] # Berre forklaring for *enkelte* variablar, men …
 )
 
@@ -70,10 +84,10 @@ enums = d %>%
 # Legg kodane inn i den nye kodeboka,
 # med rett format (heiltal for kodar
 # og tekst for kodetekst), og på rett plass
-enum_ind = kodebok$var_type == "Enum"
-kodebok$kode[enum_ind] = enums[, 1] %>%
+enum_ind = (kodebok$variabeltype == "kategorisk")
+kodebok$verdi[enum_ind] = enums[, 1] %>%
   as.numeric() # Kodar
-kodebok$kode_tekst[enum_ind] = enums[, 2] # Tilhøyrande tekst
+kodebok$verdi_tekst[enum_ind] = enums[, 2] # Tilhøyrande tekst
 
 # Sjå på den nye, flotte kodeboka
 # View(kodebok)
