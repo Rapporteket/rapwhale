a8b41737b27cc5932d0ffa42a7c36554600fab30
Lena Kristin Bache-Mathiesen <lenakba@users.noreply.github.com>
2017-05-16 15:03:18 +0000

    fjernet tegn som kodingen ikke forstod (selv om den var utf-8)
    SVN-revisjon: 1499

--- felles/r-kode/felles_funksjoner.R
+++ felles/r-kode/felles_funksjoner.R
@@ -1,6 +1,38 @@
 # Dette skriptet inneholder en rekke funksjoner som er potensielt nyttige
 # (og noen, uunnværlige) i registersammenheng og i andre sammenheng.
 
+# grafobjekter ----------------------------------------------------------
+# de offisielle fargene (som eg ikkje er så glad i)
+# du mener, som INGEN liker.
+library(ggplot2)
+
+colPrim = c("#000059", "#084594", "#2171b5", "#4292c6", "#6baed6", "#c6dbef") # Primærfarge (mørk til lys)
+colNoyt = c("#4D4D4D", "#737373", "#A6A6A6", "#DADADA") # Nøytralfarge
+colKontr = "#FF7260" # Kontrastfarge
+
+# ggplot2-tema for figurar
+tema = theme_light(base_size = 13)
+tema$panel.grid.minor$colour = "white"
+tema$strip.background$fill = "#f3f1ee"
+tema$strip.background$colour = "#e4e0da"
+tema$strip.text.x$colour = "black"
+tema$panel.spacing = unit("13", "pt")
+tema$panel.border$colour = tema$strip.background$colour
+tema$panel.grid.major$colour = tema$strip.background$colour
+tema$panel.grid.minor$colour = tema$strip.background$fill
+tema$axis.title.y$angle = 0
+tema$axis.title.y$margin = margin(r = 5)
+tema$axis.title.x$margin = margin(t = 5)
+
+# Fjern vannrette eller loddrette rutenett
+fjern_x = theme(
+  panel.grid.major.x = element_blank(),
+  panel.grid.minor.x = element_blank()
+)
+fjern_y = theme(
+  panel.grid.major.y = element_blank(),
+  panel.grid.minor.y = element_blank()
+)
 
 # Graffunksjoner ----------------------------------------------------------
 
@@ -227,7 +259,7 @@ num = function(x, desimalar) {
 
 # Tar inn eit desimaltal og viser det som prosent,
 # med mellomrom før prosentteiknet (slik det skal vera
-# på norsk), eks. 0.5 <U+2192> "50 %".
+# på norsk), eks. 0.5 --> "50 %".
 prosent = function(x) {
   stringr::str_replace(scales::percent(x), "%$", " %")
 }
