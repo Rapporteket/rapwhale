f33e28026b9b65025f80355e17b0c902c99cc670
Karl Ove Hufthammer <karl.ove.hufthammer@helse-bergen.no>
2018-02-14 14:49:05 +0000

    Lagt til ein haug med kontroll på at innargumenta er OK
    SVN-revisjon: 2482

--- felles/pakker/rapwhale/R/ekstern_validering.R
+++ felles/pakker/rapwhale/R/ekstern_validering.R
@@ -1,13 +1,8 @@
 # Skript for å hente ut tilfeldige celler fra datasett til validering.
 
-# Nødvendige pakkar
-library(tidyverse)
-library(haven)
-library(rlang)
-
 #' Lag valideringsdatasett frå datarammer.
 #'
-#' Dette er eit sett funksjonar for å henta ut (tilfeldige) data
+#' Eit sett funksjonar for å henta ut (tilfeldige) data
 #' frå eit registerdatasett og lagra desse som SPSS-filer ein kan
 #' bruka til ekstern validering av registeret.
 #'
@@ -35,6 +30,10 @@ library(rlang)
 #'   Typiske eksempel er fødselsdato, kjønn og operasjonsdato.
 #'   Rekkjefølgja i valideringsdatasetta vert lik rekkjefølgja oppgitt her
 #'   (som kan vera forskjellig frå rekkjefølgja i \code{df}).
+#' @param sjukehus_var Tekstvektor med namna på variablane
+#'   som unikt identifiserer sjukehuset/avdelingen som rada tilhøyrer,
+#'   typisk sjukehusnamn og/eller RESH-ID. Verdiane vert brukte til
+#'   å namngje utdatafilene.
 #' @param data_var Tekstvektor med namna på datavariablane
 #'   i registeret som kan plukkast ut som tilfeldige variablar.
 #'   Viss denne er \code{NULL} (standard), vert alle variablar i \code{df}
@@ -42,34 +41,93 @@ library(rlang)
 #'   I valideringsdatasettet vert kvar tilfeldig utplukka celle
 #'   til ei rad, og radene vert sorterte i rekkjefølgja som
 #'   variabelnamna er oppgitt i \code{data_var}.
-#' @param sjukehus_var Tekstvektor med namna på variablane
-#'   som unikt identifiserer sjukehuset/avdelingen som rada tilhøyrer,
-#'   typisk sjukehusnamn og/eller RESH-ID. Verdiane vert brukte til
-#'   å namngje utdatafilene.
 #' @param nvar Talet på datavariablar som skal plukkast tilfeldig frå
 #'   kvar rad i \code{df}. Set denne til lengda av \code{data_var}
 #'   dersom du ikkje ønskjer tilfeldig utplukk men heller vil
 #'   kontrollera \emph{alle} datavariablane for kvar kjelderad.
 #' @param utmappe Mappa valideringsfilene skal lagrast i.
 #'   fixme: Skil ut som eigen funksjon.
-hent_validering_data = function(df, indeks_var, ekstra_var = NULL, data_var, sjukehusvar, vdatamappe, nvar = 5) {
+hent_validering_data = function(df, indeks_var, ekstra_var = NULL, sjukehus_var,
+                                data_var = NULL, nvar = 5) {
 
-  # fixme: sjekk at indeks_var unikt identifiserer rader
-  # fixme: sjekk at indeks_var ikkje er med i data_var eller ekstra_var
+  # Elementær datasjekk -----------------------------------------------------
+
+  # Me sjekkar ikkje alt mogleg, men dei viktigaste tinga,
+  # der det er lett å blingsa. For andre ting dukkar det
+  # gjerne opp feilmeldingar automatisk (for eksempel viss
+  # 'nvar' har meir enn eitt element).
+
+  # Variablane som faktisk finst i datasettet
+  df_var = names(df)
+
+  # Viss ein ikkje har valt datavariablar,
+  # bruk alle som ikkje er indeksvariablar
+  data_vars = setdiff(df_vars, indeks_var)
+
+  # Sjekk at alle variabelsetta faktisk finst i datasettet
+  stopifnot(
+    all(indeks_var %in% df_var),
+    all(ekstra_var %in% df_var),
+    all(sjukehus_var %in% df_var),
+    all(data_var %in% df_var)
+  )
+
+  # Sjekk at ingen av variabellistene har duplikatverdiar
+  # (kunne føra til problem seinare, ikkje minst for testane)
+  stopifnot(
+    !anyDuplicated(indeks_var),
+    !anyDuplicated(ekstra_var),
+    !anyDuplicated(sjukehus_var),
+    !anyDuplicated(data_var)
+  )
+
+  # Sjekk at indeks_var utgjer ein ekte primærnøkkel
+  indeks_var_q = syms(indeks_var)
+  if (any(duplicated(select(df, !!!indeks_var_q)))) {
+    stop("'indeks_var' identifiserer ikkje radene unikt")
+  }
+
+  # Rams opp alle elementa i ein vektor,
+  # skilde med komma og med ' rundt seg
+  # fixme: flytta denne ut av funksjonen, til ein
+  #        global, *ikkje-eksportert* funksjon?
+  lag_liste = function(x) {
+    paste0(paste0("'", indeks_i_ekstra, "'"), collapse = ", ")
+  }
+
+  # Sjekk at indeksvariablar ikkje blir brukt andre plassar
+  indeks_i_ekstra = intersect(indeks_var, ekstra_var)
+  if (length(indeks_i_ekstra) > 0) {
+    stop(
+      "'indeks_var' har variablar som òg finst i 'ekstra_var':\n  ",
+      lag_liste(indeks_i_ekstra)
+    )
+  }
+  indeks_i_data = intersect(indeks_var, data_var)
+  if (length(indeks_i_data) > 0) {
+    stop(
+      "'indeks_var' har variablar som òg finst i 'data_var':\n  ",
+      lag_liste(indeks_i_data)
+    )
+  }
+
+  if (nvar > length(data_vars)) {
+    stop(
+      "Kan ikkje plukka ut fleire tilfeldige variablar (", nvar, ") ",
+      "enn det finst datavariablar (", length(data_vars), ")."
+    )
+  }
+
+  # fixme: Lag testar (test_that()) for alle feilmeldingane våre.
+  # fixme: Lag testar for at ting fungerer *riktig*. :)
   # fixme: ha med 'valideringsgruppenummer' i resultatdatasettet.
-  # fixme: sjekk at nvars <= length(data_vars).
-  # fixme: sjekk at indeks_var og ekstra_var ikkje har overlapp (ev. handtera dette).
-  # fixme: handtering av blinding
+  # fixme: handtering av blinding (vising/gøyming av 'reg_*'-kolonnane)
   # fixme: gje ut berre maks_eitelleranna pasientar/opphald i utdatarammene.
-  # fixme: fungerer sjukehus_var med lengd > 1
-  # fixme: sjekk at alle obligatoriske variablar faktisk finst og handter dupliserte verdiar (via unique()).
+  # fixme: sjekk at sjukehus_var med lengd > 1 fungerer
   # fixme: reformater kjeldekoden (innrykk og sånt)
   # fixme: del funksjonen opp i éin funksjon for å laga valideringsdatasetta og éin funksjon for å lagra til SPSS-format.
 
-  # Sjekk at alle indeksvariablane faktisk finst i datasettet
-  stopifnot(all(indeks_vars %in% names(d)))
-  data_vars = valid_vars %>%
-    setdiff(indeks_vars)
+
 
   # quote-sjukehusvariabel
   sjukehusvar = sym(sjukehusvar)
@@ -162,25 +220,30 @@ hent_validering_data = function(df, indeks_var, ekstra_var = NULL, data_var, sju
   shell.exec(vdatamappe)
 }
 
+df = as.tibble(mice::selfreport)
+indeks_var = c("id", "edu")
+ekstra_var = c("age", "sex")
+data_var = c("hr", "wr", "etn", "br")
+sjukehus_var = c("etn", "web")
+utmappe = "h:/tmp/greier"
+
+
 # tester funksjonen under
-
-# Oversikt over namn på indeksvariablar og datavariablar
-indeks_vars_soreg = c(
-  "PasientID", "Fodselsdato", "PasientAlder", "PasientKjonn",
-  "OpererendeRESH", "ForlopsID", "OperasjonsID", "OperererendeSykehus"
-)
-# mappe for valideringsdata
-vmappe = paste0(grunnmappe, "..\\valideringsdata\\", Sys.Date(), "\\")
-
-# variabler man ønsker å validere
-# husk at nvars må være >= antall valideringsvariabler (kolonner)
-soreg_vars = names(d)
-
-# kjør funksjonen
-hent_validering_data(d,
-  nvars = 10,
-  indeks_vars = indeks_vars_soreg,
-  valid_vars = soreg_vars,
-  sjukehusvar = "OperererendeSykehus",
-  vdatamappe = vmappe
-)
+#
+# # Oversikt over namn på indeksvariablar og datavariablar
+# indeks_vars_soreg=c("PasientID", "Fodselsdato", "PasientAlder", "PasientKjonn",
+#            "OpererendeRESH", "ForlopsID", "OperasjonsID", "OperererendeSykehus")
+# # mappe for valideringsdata
+# vmappe = paste0(grunnmappe, "..\\valideringsdata\\", Sys.Date(), "\\")
+#
+# # variabler man ønsker å validere
+# # husk at nvars må være >= antall valideringsvariabler (kolonner)
+# soreg_vars = names(d)
+#
+# # kjør funksjonen
+# hent_validering_data(d,
+#                      nvars = 10,
+#                      indeks_vars = indeks_vars_soreg,
+#                      valid_vars = soreg_vars,
+#                      sjukehusvar = "OperererendeSykehus",
+#                      vdatamappe = vmappe)
