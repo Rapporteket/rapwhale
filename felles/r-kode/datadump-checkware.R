7a0d9e2393f7553a2775bcba0d2b04dabc1f784c
Lena Kristin Bache-Mathiesen <lenakba@users.noreply.github.com>
2018-05-14 13:40:55 +0000

    Laget kode for å hente inn datadump med riktig variabeltype basert på kodeboka
    SVN-revisjon: 2716

--- felles/r-kode/datadump-checkware.R
+++ felles/r-kode/datadump-checkware.R
@@ -85,26 +85,89 @@ variabel_id_checkware = kb %>%
 kb_kanonisk = kb_kanonisk %>%
   left_join(variabel_id_checkware, by = "variabel_id")
 
-variabelnamn = kb_kanonisk %>%
+# Me skil berre mellom heiltals- og flyttalsvariablar
+# i vår kodebok ved hjelp av «desimalar»-feltet (begge
+# talvariantane har variabeltypen «numerisk»). For å
+# kunna handtera dette riktig (les: strengt) ved
+# innlesing av data, legg me derfor til ein kunstig
+# «numerisk_heiltal»-variabeltype.
+kb_kanonisk = kb_kanonisk %>%
+  mutate(variabeltype = replace(
+    variabeltype,
+    (variabeltype == "numerisk") & (desimalar == 0),
+    "numerisk_heiltal"
+  ))
+
+# henter ut variabelnavn for metadata som er i hvert skjema +
+# for aktuelt skjema, og variabeltype
+var_info = kb_kanonisk %>%
   filter(skjema_id == "meta" | skjema_id == "barthel") %>%
-  distinct(variabel_id, variabel_id_checkware)
+  distinct(variabel_id, variabel_id_checkware, variabeltype)
 
-# henter inn data
-filnamn = "barthel.csv"
-
-d_barthel = read_delim(paste0(mappe, filnamn), delim = ";", na = "")
-
-read_delim(
-  adresse,
-  delim = ";",
-  na = "null",
-  locale = locale(date_format = "%d.%m.%Y", decimal_mark = ","),
+# Forkortingsbokstavane som read_csv() brukar (fixme: utvide med fleire)
+# fixme: Kategorisk er ein litt vrien variant. *Oftast* er han
+#        tal, men me kan risikera at han er tekst òg. Ei løysing er å
+#        alltid lesa han inn som tekst, men det er ikkje ei *god* løysing.
+#        Når han er koda som tal, er det ofte betre å handsama han som tal.
+#        Det ser betre ut, og det mogleggjer å bruka operatorar som < og >=
+#        (eks. komplikasjonsgrad < 5) (men "10" er som kjent < "2"!).
+#        Og for spørjeskjema er gjerne skåringskodane lagt inn som talkodar,
+#        slik at det er fint om me kan skriva for eksempel sp1 + sp2 + ...
+#        for å få ein sumskår).
+#
+#        Så den *rette* måten å handtera dette på er å lesa inn kategoriske
+#        verdiar som tal dersom dei moglege *verdiane* i kodeboka alle er tal
+#        og som tekst elles.
+#
+#        Det kunne vera aktuelt å dela opp i kategorisk_numerisk og
+#        kategorisk_tekst i vår kanoniske kodebok, men det ville komplisera
+#        annan kode som brukar kodebøkene (eks.kb_fyll()), så det bør me nok
+#        helst ikkje gjera.
+#
+#        Programmeringsmessig blir anbefalt løysing litt komplisert,
+#        men det skal me få til!
+spek_csv_checkware = tribble(
+  ~variabeltype, ~csv_bokstav,
+  "kategorisk", "i",
+  "tekst", "c",
+  "boolsk", "c", # Sjå konvertering nedanfor
+  "dato_kl", "c", # Mellombels, jf. https://github.com/tidyverse/readr/issues/642 (fixme til "T" når denne er fiksa)
+  "dato", "c",
+  "numerisk", "d",
+  "numerisk_heiltal", "i"
 )
 
+spek_innlesing = var_info %>%
+  left_join(spek_csv_checkware, by = "variabeltype")
 
-kolnamn = variabelnamn$variabel_id_checkware %>%
-  setNames(variabelnamn$variabel_id)
-d = d %>%
+# Les inn datasettet
+filnamn = "barthel.csv"
+adresse = paste0(mappe, filnamn)
+
+kol_typar = str_c(spek_innlesing$csv_bokstav, collapse = "")
+d_barthel = read_delim(adresse,
+  delim = ";", na = "",
+  col_names = spek_innlesing$variabel_id,
+  quote = "\"", trim_ws = FALSE, col_types = kol_typar,
+  locale = locale(
+    decimal_mark = ",", grouping_mark = "",
+    date_format = "%d.%m.%Y", time_format = "%H:%M:%S"
+  )
+)
+
+# Datafila *kan* ikkje innehalda duplikate kolonnenamn,
+# sidan me då ikkje kan veta kva kolonne eit namn svarar til.
+# Stopp derfor viss me finn duplikate namn.
+dupnamn = duplicated(names(d_barthel))
+if (any(dupnamn)) {
+  stop(
+    "Datafila har duplikate variabelnamn:\n",
+    str_c(d_barthel[dupnamn], collapse = "\n")
+  )
+}
+
+# setter variabelnavn
+kolnamn = var_info$variabel_id_checkware %>%
+  setNames(var_info$variabel_id)
+d_barthel = d_barthel %>%
   rename(!!!kolnamn)
-
-d
