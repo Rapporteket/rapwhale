b72e11bc34a7dbbde11c5cd82dcfc85d8d246e8f
Lena Kristin Bache-Mathiesen <lenakba@users.noreply.github.com>
2018-05-14 07:14:31 +0000

    Oppdatert med en kommentar
    SVN-revisjon: 2710

---
+++ felles/r-kode/datadump-checkware.R
@@ -0,0 +1,78 @@
+# Dette skal være eit R-skript med ein funksjon for lesing av CheckWare-data og kodebok.
+#
+# Innlesingsfunksjonen bør ta inn:
+#
+# - Ei mappeadresse, der mappa inneheld kodebok og tilhøyrande datadumpfiler.
+# - Ein skjema-ID, som tilsvarer skjema-ID-en i kodeboka og filnamnet til datadumpen (men utan .csv).
+# - og returnera ein R-tibble (eller ein feil dersom ting ikkje er i orden).
+#
+# Internt lastar funksjonen inn tilhøyrande kodebok og brukar denne for å lesa tilhøyrande datadumpfil.
+# (Internt kan det vera to funksjonar, ein for lesing av kodeboka, jf. kode i h:\kvalreg\rehabiliteringsregisteret
+# og ein for lesing av datadumpfila gitt ein kodebok-tibble på kanonisk form).
+#
+# Funksjonen bør òg sjekka at kodeboka er gyldig (før bruk)
+# og at datadumpen er gyldig (etter importering),
+# og gje feilmelding (ikkje åtvaring) dersom ikkje.
+# Det bør vera mogleg å slå av desse sjekkane, men dei må vera på som standard.
+
+#---------------------------innhenting av pakker og funksjoner--------------------------------
+
+library(tidyverse) # livsnæring
+library(magrittr) # pipe-funksjon
+library(readxl)
+
+# henter funksjon for å lage kodebok til kanonisk form + kodebok valider
+source("h:/kvalreg/felles/r-kode/kodebok-valider.R", encoding = "UTF-8")
+
+#--------------------------datainnhenting - bruker rehabiliteringsregisteret som utgangspunkt------------------------
+
+# Les inn kodebok
+mappe = "h:/kvalreg/rehabiliteringsregister/"
+kb = read_excel("h:/kvalreg/rehabiliteringsregisteret/kodebok-AFMR-register 0.7.xlsx", sheet = 1)
+
+# I kodeboka til rehabiliteringsregisteret er det flere kolonner som ikke
+# er utfylt fordi det ikke er aktuelt.
+# disse har ikke klart å få riktig tolkning for variabeltype
+# Rettet datatype for nokre (tomme) variablar som vart feiltolka til feil datatype
+kb = kb %>%
+  mutate(
+    desimalar = as.integer(desimalar),
+    eining = as.character(eining),
+    kategori = skjemanamn,
+    maks = as.double(maks),
+  )
+
+# Legg til standardkolonnar som manglar
+kb = kb %>%
+  mutate(
+    innleiing = NA_character_,
+    kategori = NA_character_,
+    unik = "nei",
+    manglande = NA_character_,
+    min_rimeleg = NA_real_,
+    maks_rimeleg = NA_real_,
+    kommentar_rimeleg = NA_character_,
+    utrekningsformel = NA_character_,
+    logikk = NA_character_,
+    kommentar = NA_character_
+  )
+
+# Fiks rekkjefølgja på variablane
+std_namn = c(
+  "skjema_id", "skjemanamn", "kategori", "innleiing", "variabel_id",
+  "variabeletikett", "forklaring", "variabeltype", "eining", "unik",
+  "obligatorisk", "verdi", "verditekst", "manglande", "desimalar",
+  "min", "maks", "min_rimeleg", "maks_rimeleg", "kommentar_rimeleg",
+  "utrekningsformel", "logikk", "kommentar"
+)
+kb = kb %>%
+  select(!!std_namn)
+
+# Sjekk kodeboka
+kb_er_gyldig(kb)
+warnings()
+
+# gjør om kodeboka til kanonisk form
+kb_kanonisk = kb_til_kanonisk_form(kb)
+
+# henter inn data
