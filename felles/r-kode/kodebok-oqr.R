3880bbfd30bb75b52bbef860fc2c6bb95a568a7a
Karl Ove Hufthammer <karl.ove.hufthammer@helse-bergen.no>
2019-02-20 15:05:49 +0000

    Fiksa regexp for datouthenting. (Det gamle regexp-et klarte å vera *både* for streng og for lemfeldig på ein gong!)
    SVN-revisjon: 3705

--- felles/r-kode/kodebok-oqr.R
+++ felles/r-kode/kodebok-oqr.R
@@ -256,7 +256,7 @@ les_kb_oqr = function(mappe_dd, reg_id, dato = NULL) { # fixme: Validering av ko
 les_dd_oqr = function(mappe_dd, reg_id, skjema_id, status = 1, dato = NULL, kodebok = NULL) { # fixme: Legg på dd-validering?
   # Bruk siste tilgjengelege kodebok dersom ein ikkje har valt dato
   if (is.null(dato)) {
-    dato = dir(mappe_dd, pattern = "[0-9]{4}-[0-1]{2}-[0-9]{2}", full.names = FALSE) %>%
+    dato = dir(mappe_dd, pattern = "^[0-9]{4}-[0-1][0-9]-[0-9]{2}$", full.names = FALSE) %>%
       sort() %>%
       last()
   }
