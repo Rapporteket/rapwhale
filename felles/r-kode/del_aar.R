8c2be6fd1eb67a8674050e61abe89ece9409d1b7
Endre Kvåle Evjen <endre.kvale.evjen@helse-bergen.no>
2019-08-16 09:06:55 +0000

    Lagt til ny funksjonalitet hvor klokkeslett inkluderes i beregningen av intervall. Default klokkeslett om det mangler er 12:00, ellers brukes det som måtte være angitt. Har også oppdatert testene
    SVN-revisjon: 4444

--- felles/r-kode/del_aar.R
+++ felles/r-kode/del_aar.R
@@ -1,101 +1,81 @@
 
 # Konvertere tid til punkter på en akse:
 library(lubridate)
-library(tidyverse)
-
 
 # Funksjonen tar inn en vektor med årstall, en vektor med periode-tilhørighet og et heltall som sier hvor mange deler året skal deles opp i.
+# Utdata er en vektor med desimaltall som representerer de ulik periodene i inndata.
+
 del_aar = function(aar, delnummer, antall_deler) {
 
   # Sjekk at inndata er i riktig format
   stopifnot(length(aar) == length(delnummer))
   if (any(is.na(aar))) {
     warning("År inneholder NA-verdier")
-  } else if (any(is.na(delnummer))) {
+  }
+  if (any(is.na(delnummer))) {
     warning("Delnummer inneholder NA-verdier")
   }
-  if (any(na.omit(aar) != floor(na.omit(aar)))) {
-    stop("År er ikke heltall")
-  }
-  if (antall_deler != floor(antall_deler)) {
-    stop("Antall deler er ikke et heltall.")
+  if (antall_deler < 1) {
+    stop("Antall deler er < 1")
   }
 
-  # Regne ut intervall-bredde, og midtpunkt for hvert intervall
-  bredde = 1 / antall_deler
-  endepunkt = c(0:antall_deler * bredde)
-
-  midtpunkt = vector(mode = "numeric", length = (length(endepunkt) - 1))
-  for (i in 1:length(endepunkt) - 1) {
-    midtpunkt[i] = (endepunkt[i] + endepunkt[i + 1]) / 2
-  }
-
-  (aar + midtpunkt[delnummer])
+  (aar + delnummer / antall_deler - (1 / antall_deler) / 2)
 }
 
 
 # Funksjonen lag_periode tar inn en vektor med datoer og et heltall som sier hvor mange deler året skal deles opp i.
-# Den kan ta et vilkårlig antall deler, men vil ved antall = 2, 4 eller 12 benytte seg av del_aar for å ivareta korrekt fordeling av datoer rundt periode-skift. Dette gjøres siden forskjellig antall dager i mnd/kvartal/halvår håndteres bedre av month()- og quarter()-funksjon i lubridate.
 
 lag_periode = function(dato, antall_deler) {
-  if (!(is.Date(dato))) {
-    stop("Dato-vektor er ikke i Date-format")
+
+  # Sjekke inndata
+  if (!(is.timepoint(dato))) { # TRUE for både Date og POSIXt-format
+    stop("Dato-vektor er ikke i Date- eller POSIXt-format")
   }
 
-  if (antall_deler %in% c(2, 4, 12)) {
+  # er dato i Date format settes klokkeslett til 12:00 (konverterer dato til POSIXlt)
+  if (is.Date(dato)) {
+    hour(dato) = 12
 
-    # Halv-års-inndeling:
-    if (antall_deler == 2) {
-      delnummer = case_when(
-        month(dato) <= 6 ~ 1,
-        month(dato) > 6 ~ 2,
-        TRUE ~ NA_real_
-      )
-    }
-
-    # Kvartal
-    if (antall_deler == 4) {
-      delnummer = quarter(dato)
-
-      # Måned
-    } else if (antall_deler == 12) {
-      delnummer = month(dato)
-    }
-
-    del_aar(
-      aar = year(dato),
-      delnummer = delnummer,
-      antall_deler = antall_deler
-    )
+    # Hvis inndata ikke er Date må det nødvendigvis være POSIX*t format.
+    # Av disse er det lettest å bruke POSIXlt format for å identifisere om det inneholder et klokkeslett.
+    # Alle POSIxlt konverteres til POSIXlt før vi sjekker om det eksisterer timer, minutt eller sekunder registrert.
+    # Finnes det setter vi tidssone til UTC.
+    # Finnes ingen klokkeslett setter vi det til 12:00
   } else {
-    if (any(is.na(dato) == TRUE)) {
-      warning("Det finnes NA-verdier i dato-vektor")
-    } # Vil helst bruke tid_1 i disse tilfellene, da det er naturlige avgrensninger for de forskjellige tids-periodene, med ujevnt antall dager i hver periode.
-
-    # Finne intervaller og midtpunkt for hver tids-periode
-    bredde = 1 / antall_deler
-    endepunkt = c(1:antall_deler * bredde) # mangler 0 for første år.
-
-    # Midtpunkt
-    unike_ar = sort(unique(na.omit(year(dato))))
-    aar = c(min(unike_ar), endepunkt + rep(unike_ar, each = length(endepunkt)))
-    aar_midtpunkt = vector(mode = "numeric", length = (length(aar) - 1))
-
-    for (i in 1:length(aar_midtpunkt)) {
-      aar_midtpunkt[i] = (aar[i] + aar[i + 1]) / 2
+    dato = as.POSIXlt(dato)
+    if (any(unlist(dato[1])[1:3] != 0)) {
+      tz(dato) = "UTC"
+    } else {
+      hour(dato) = 12
     }
-
-    # Finner hvilket intervall hver observasjon tilhører.
-    nye_pkt = findInterval(x = decimal_date(dato), vec = aar)
-
-    # Returnerer utverdi
-    (aar_midtpunkt[nye_pkt])
   }
+
+  if (any(is.na(dato) == TRUE)) {
+    warning("Det finnes NA-verdier i dato-vektor")
+  }
+
+
+  # Finne midtpunkt for alle årstall som er representert i dato.
+  endepunkt = 1:antall_deler * (1 / antall_deler)
+
+  unike_ar = sort(unique(year(dato)))
+  aar = c(min(unike_ar), endepunkt + rep(unike_ar, each = length(endepunkt)))
+
+  aar_midtpunkt = numeric(length = length(unike_ar) * antall_deler)
+  for (i in 1:length(aar_midtpunkt)) {
+    aar_midtpunkt[i] = (aar[i] + aar[i + 1]) / 2
+  }
+
+  # Finner hvilket intervall hver observasjon tilhører.
+  nye_pkt = findInterval(x = decimal_date(dato), vec = aar, rightmost.closed = FALSE, left.open = FALSE) # [2019,2020)
+
+  # Returnerer utverdi
+  (aar_midtpunkt[nye_pkt])
 }
 
 # TESTER
 library(testthat)
-test_adr = "H:\\R\\R_scripts\\Ny mappe\\tidslinje_tester.R"
+test_adr = "H:\\kvalreg\\felles\\R-kode\\del_aar_tester.R"
 test_file(test_adr, reporter = "minimal") # *Veldig* kort og konsist samandrag
 test_file(test_adr, reporter = "check") # 13-linjes samandrag
 test_file(test_adr, reporter = "summary") # Alt, med fint samandrag i starten
--- felles/r-kode/del_aar_tester.R
+++ felles/r-kode/del_aar_tester.R
@@ -1,105 +1,27 @@
 # tester for tidslinje-funksjon:
 
-# Testdata ----------------------------------------------------------------
-
-# Enkeltstående år
-ar = 2019
-
-# Vektor med årstall
-arevis = c(2019:2021)
-
-# Vektor med manglende årstall
-ar_med_hull = c(2019, 2020, 2022, 2023)
-
-# Vektor med NA årstall
-ar_med_na = c(2019, 2020, NA, 2022)
-
-# Vektor med desimal-årstall:
-ar_med_des = c(2019, 2020.5, NA, 2022)
-
-# Vektor med årstall i tekst:
-ar_med_tekst = c("tjueatten", "tjuenitten", "tjuetjue")
-
-# antall deler:
-ad = 4
-ad_desi = 4.2
-ad_desi_hel = 4.0
-
-# Dato vektor, daglig:
-dagevis = seq.Date(from = as.Date("2018/1/1"), as.Date("2019/10/10"), "days")
-ukevis = seq.Date(from = as.Date("2018/1/1"), as.Date("2019/10/10"), "weeks")
-mndvis = seq.Date(from = as.Date("2018/1/1"), as.Date("2019/10/10"), "months")
-
-
-# Dato-uttrekk, inkludert 0101 og 3112:
-date = c(seq.Date(from = as.Date("2018-01-01"), to = as.Date("2019-01-02"), "days"))
-
-dato_sample_m_nyttar = c(
-  as.Date("2019-01-01"), sample(date, size = 15), as.Date("2019-12-31"),
-  as.Date("2020-01-01"), as.Date("2020-01-02")
-)
-
-# Tilfeldige datoer fra flere år
-dato_arevis = sample(seq.Date(from = as.Date("2019-01-01"), to = as.Date("2025-12-31"), "days"), size = 30)
-dato_diger = sample(seq.Date(from = as.Date("2019-01-01"), to = as.Date("2025-12-31"), "days"), size = 3000, replace = TRUE)
-
-# Dato fra flere år hvor alle mangler OKT, NOV, DES.
-dato_ar_m_hull = c(
-  sample(seq.Date(from = as.Date("2019-01-01"), to = as.Date("2019-09-30"), "days"), size = 10),
-  sample(seq.Date(from = as.Date("2020-01-01"), to = as.Date("2020-09-30"), "days"), size = 10),
-  sample(seq.Date(from = as.Date("2021-01-01"), to = as.Date("2021-09-30"), "days"), size = 10)
-)
-
-# Dato med NA-verdier:
-dato_m_na = c(
-  sample(seq.Date(from = as.Date("2019-01-01"), to = as.Date("2019-12-31"), "days"), size = 30),
-  c(NA, NA, NA)
-)
-
-# Dato med skuddår:
-dato_m_skudd = c(
-  sample(seq.Date(from = as.Date("2020-01-01"), to = as.Date("2020-12-31"), "days"), size = 300),
-  as.Date("2020-02-29")
-)
-
-# Dato rundt halvårs-skifte:
-dato_halv = seq.Date(from = as.Date("2019-05-01"), to = as.Date("2019-07-31"), "days")
-
-# Generere datasett som tilsvarer hva en ser i en database:
-ar_i_db = sample(x = c(2017, 2018, 2019), size = 10, replace = TRUE)
-ar_i_db_diger = sample(x = c(2017, 2018, 2019, 2020), size = 1000, replace = TRUE)
-ar_med_gap = c(rep(2017:2019, each = 4))
-ar_i_db_na = sample(x = c(NA_integer_, 2018, 2019), size = 10, replace = TRUE)
-ar_m_des = c(sample(x = c(2017, 2018, 2019), size = 9, replace = TRUE), 2018.5)
-ar_m_des2 = c(sample(x = c(2017, 2018, 2019), size = 9, replace = TRUE), 2018.0)
-
-# Delnummer-vektor hentet fra database:
-kvart_i_db = sample(x = 1:4, size = 10, replace = TRUE)
-kvart_i_db_diger = sample(x = 1:4, size = 1000, replace = TRUE)
-kvart_med_gap = c(1, 2, 3, 4, 1, 3, 3, 4, 1, 2, 3, 4)
-kvart_uten_3 = c(1, 2, 2, 4, 1, 2, 4, 1, 2, 4)
-kvart_uten_4 = c(1, 2, 2, 3, 1, 2, 3, 1, 2, 3)
-kvart_i_db_na = c(sample(x = 1:4, size = 9, replace = TRUE), NA_integer_)
-mnd_i_db = sample(x = 1:12, size = 10, replace = TRUE)
-
-
-
 # Tester - Funksjon 1 -----------------------------------------------------
 
 context("Funksjon 1 - inndata")
 
 # Sjekk at det gis feilmelding om årstall inneholder desimal:
 test_that("Det gis feilmelding om årstall-vektor ikke er heltallsvektor", {
+  ar_m_des = c(2017, 2018, 2019, 2017, 2015, 2010, 2020, 2018.5)
+  kvart_i_db = c(1, 2, 3, 4, 1, 2, 3, 1, 2, 3)
   expect_error(del_aar(ar_m_des, kvart_i_db, 4))
 })
 
-# Sjekk at det gis feilmelding om antall_deler ikke er et heltall.
-test_that("Det gis feilmelding om antall_deler ikke er et heltall", {
-  expect_error(del_aar(ar_i_db, kvart_i_db, 4.6), "Antall deler er ikke et heltall.")
+# Sjekk at det gis feilmelding om antall_deler er mindre enn 1..
+test_that("Det gis feilmelding om antall_deler er mindre enn 1.", {
+  ar_i_db = c(2015, 2020, 2015, 2013, 2019, 2020, 2015, 2019, 2018, 2017)
+  kvart_i_db = c(1, 2, 3, 4, 1, 2, 3, 1, 2, 3)
+  expect_error(del_aar(ar_i_db, kvart_i_db, 0.5), "Antall deler er < 1")
 })
 
 # Sjekk at funksjonen gir en advarsel om årstall inneholder NA:
 test_that("Det gis advarsel om det finnes NA-verdier i årstall", {
+  ar_i_db_na = c(2017, 2018, 2019, NA_integer_, 2020, 2018, 2015, 2020, 2019, 2270)
+  kvart_i_db = c(1, 2, 3, 4, 1, 2, 3, 1, 2, 3)
   expect_warning(del_aar(ar_i_db_na, kvart_i_db, 4), "År inneholder NA-verdier",
     fixed = TRUE
   )
@@ -109,14 +31,39 @@ context("Funksjon 1 - Utdata")
 
 # Utdata er aldri heltall, selv med flere enn 365 deler.
 test_that("Utdata aldri er et heltall", {
-  expect_false(any(del_aar(ar_i_db_diger, kvart_i_db_diger, 4) == floor(del_aar(ar_i_db_diger, kvart_i_db_diger, 4))))
+  ar_i_db_diger = sample(x = c(2017, 2018, 2019, 2020), size = 1000, replace = TRUE)
+  kvart_i_db_diger = sample(x = 1:4, size = 1000, replace = TRUE)
+  # expect_false(any(del_aar(ar_i_db_diger, kvart_i_db_diger, 4) == floor(del_aar(ar_i_db_diger, kvart_i_db_diger, 4))))
+  expect_false(isTRUE(del_aar(ar_i_db_diger, kvart_i_db_diger, 4) == floor(del_aar(ar_i_db_diger, kvart_i_db_diger, 4))))
 })
+
+# Utdata samsvarer med forventet utverdi.
+test_that("Utdata samsvarer med forventet resultat.", {
+  aar = rep(2019, 4)
+  kvart = c(1, 2, 3, 4)
+  mnd = c(1, 7, 2, 12)
+  forvent = c(2019.125, 2019.375, 2019.625, 2019.875)
+  forvent12 = c(2019 + (1 / 12) * mnd - 1 / 24)
+  expect_identical(del_aar(aar, kvart, 4), forvent)
+  expect_equal(del_aar(aar, mnd, 12), forvent12)
+})
+
 
 context("Funksjon 1 - Grensetilfeller")
 
 # Få observasjoner i årstall
 test_that("Funksjonen fungerer med kun ett årstall", {
   expect_equal(del_aar(2019, 1, 4), 2019.125)
+})
+
+# Funksjonen fungerer med kun 1 del.
+test_that("Funksjonen fungerer med kun en del", {
+  expect_equal(del_aar(2019, 1, 1), 2019.5)
+})
+
+# Funksjonen fungerer med 366 deler
+test_that("Funksjonen fungerer med 366 deler", {
+  expect_equal(del_aar(2019, 1, 366), 2019 + (1 / 366 / 2))
 })
 
 # Tester - Funksjon 2 -----------------------------------------------------
@@ -126,39 +73,127 @@ context("Funksjon 2 - inndata")
 
 # Sjekk at funksjonen gir en advarsel om dato-vektor inneholder NA:
 test_that("Det gis advarsel om det finnes NA-verdier i datovektor", {
+  dato_m_na = c(sample(seq(from = as.Date("2019-01-01"), to = as.Date("2021-12-31"), "days"), size = 30), NA, NA, NA)
   expect_warning(lag_periode(dato_m_na, 3), "Det finnes NA-verdier i dato-vektor",
     fixed = TRUE
   )
 })
 
-
-# Kjører del_aar gitt antall_deler er 4 eller 12.
-# Forventer at del_aar blir kalt med de ønskede lubridatefunksjonene for å hente ut riktig delnummer fra hver dato.
-test_that("Det gis samme output når antall_deler er 4 for lag_periode og del_aar", {
-  expect_identical(lag_periode(dato_arevis, 4), del_aar(year(dato_arevis), quarter(dato_arevis), 4))
-})
-
-# Dele opp i mnd
-test_that("Det gis samme output når antall_deler er 12 lag_periode og del_aar", {
-  expect_identical(lag_periode(dato_arevis, 12), del_aar(year(dato_arevis), month(dato_arevis), 12))
-})
-
 # Dato-vektor er ikke Date-format.
 test_that("Det gis feilmelding om dato ikke er i Date-format", {
-  expect_error(lag_periode(ar_i_db, 12), "Dato-vektor er ikke i Date-format")
+  ar_i_db = c(2015, 2020, 2015, 2013, 2019, 2020, 2015, 2019, 2018, 2017)
+  ar_i_char = c("2015", "2020", "2015", "2013", "2019", "2020", "2015", "2019", "2018", "2017")
+  expect_error(lag_periode(ar_i_db, 12), "Dato-vektor er ikke i Date- eller POSIXt-format")
+  expect_error(lag_periode(ar_i_char, 12), "Dato-vektor er ikke i Date- eller POSIXt-format")
 })
+
+# Ut-data er forskjellig for ulike klokkeslett.
+test_that("Det gis ulike resultat ved forskjellige klokkeslett", {
+  dato_tidlig = as.POSIXlt(c("2019-01-01 02:00:00", "2019-04-01 02:00:00", "2019-08-01 02:00:00", "2019-12-01 02:00:00"))
+  dato_sent = as.POSIXlt(c("2019-01-01 21:00:00", "2019-04-01 21:00:00", "2019-08-01 21:00:00", "2019-12-01 21:00:00"))
+  expect_false(any(lag_periode(dato_tidlig, 730) == lag_periode(dato_sent, 730)))
+})
+
+
+# Decimal_date gir ulik ut-data basert på klokkeslett i dato-vektor.
+test_that("Decimal_date gir ulik ut-data ved ulike klokkeslett i dato-vektor. ", {
+  dato_tidlig = as.POSIXlt(c("2019-01-01 02:00:00", "2019-04-01 02:00:00", "2019-08-01 02:00:00", "2019-12-01 02:00:00"))
+  dato_sent = as.POSIXlt(c("2019-01-01 21:00:00", "2019-04-01 21:00:00", "2019-08-01 21:00:00", "2019-12-01 21:00:00"))
+
+  dato_tidlig_ct = as.POSIXct(c("2019-01-01 02:00:00", "2019-04-01 02:00:00", "2019-08-01 02:00:00", "2019-12-01 02:00:00"))
+  dato_sent_ct = as.POSIXct(c("2019-01-01 21:00:00", "2019-04-01 21:00:00", "2019-08-01 21:00:00", "2019-12-01 21:00:00"))
+
+  expect_false(any(decimal_date(dato_tidlig) == decimal_date(dato_sent)))
+})
+
+
+# Sjekke at vi får samme resultat med Dato-vektor og ingen klokkeslett og Dato-vektor med klokkeslett = 12.
+test_that("Vi får samme resultat med dato-vektor uten klokkeslett, og dato vektor med klokkeslett = 12", {
+  dato_klokke = as.POSIXlt(c("2019-01-01 12:00:00", "2019-04-01 12:00:00", "2019-08-01 12:00:00", "2019-12-01 12:00:00"))
+  dato_u_klokke = as.Date(c("2019-01-01", "2019-04-01", "2019-08-01", "2019-12-01"))
+  expect_equal(lag_periode(dato_u_klokke, 366), lag_periode(dato_klokke, 366))
+  expect_equal(lag_periode(dato_u_klokke, 3), lag_periode(dato_klokke, 3))
+  expect_equal(lag_periode(dato_u_klokke, 4), lag_periode(dato_klokke, 4))
+})
+
+# Sjekke at vi får ønsket utverdi når dato er i POSIXct format UTEN klokkeslett
+test_that("Vi får ønsket utverdi når dato er i POSIXct-format uten klokkeslett", {
+  dato_ct_uten_klokke = as.POSIXct(c("2019-01-01", "2019-04-01", "2019-08-01", "2019-12-01"))
+  dato_Date = as.Date(c("2019-01-01", "2019-04-01", "2019-08-01", "2019-12-01"))
+  expect_identical(lag_periode(dato_ct_uten_klokke, 5), lag_periode(dato_Date, 5))
+})
+# Sjekke at vi får ønsket utverdi når dato er i POSIXct format MED klokkeslett
+test_that("Vi får ønsket utverdi når dato er i POSIXct-format uten klokkeslett", {
+  dato_ct_med_klokke = as.POSIXct(c("2019-01-01 12:00", "2019-04-01 12:00", "2019-08-01 12:00", "2019-12-01 12:00"))
+  dato_ct_med_klokke_diff = as.POSIXct(c("2019-01-01 02:00", "2019-04-01 02:00", "2019-08-01 02:00", "2019-12-01 02:00"))
+  dato_Date = as.Date(c("2019-01-01", "2019-04-01", "2019-08-01", "2019-12-01"))
+
+  expect_identical(lag_periode(dato_ct_med_klokke, 500), lag_periode(dato_Date, 500)) # Klokken 12 blir likt for begge typer
+  expect_true(any(lag_periode(dato_ct_med_klokke_diff, 500) != lag_periode(dato_ct_med_klokke, 500))) # Forskjellig klokkeslett gir forskjellig utverdi gitt korte nok interval.
+})
+# Sjekke at vi får ønsket utverdi når dato er i POSIXlt format UTEN klokkeslett
+test_that("Vi får ønsket utverdi når dato er i POSIXlt-format uten klokkeslett", {
+  dato_lt_uten_klokke = as.POSIXlt(c("2019-01-01", "2019-04-01", "2019-08-01", "2019-12-01"))
+  dato_Date = as.Date(c("2019-01-01", "2019-04-01", "2019-08-01", "2019-12-01"))
+
+  expect_identical(lag_periode(dato_lt_uten_klokke, 5), lag_periode(dato_Date, 5))
+})
+# Sjekke at vi får ønsket utverdi når dato er i POSIXlt format MED klokkeslett
+test_that("Vi får ønsket utverdi når dato er i POSIXlt-format uten klokkeslett", {
+  dato_lt_med_klokke = as.POSIXlt(c("2019-01-01 12:00", "2019-04-01 12:00", "2019-08-01 12:00", "2019-12-01 12:00"))
+  dato_lt_med_klokke_diff = as.POSIXlt(c("2019-01-01 02:00", "2019-04-01 02:00", "2019-08-01 02:00", "2019-12-01 02:00"))
+  dato_Date = as.Date(c("2019-01-01", "2019-04-01", "2019-08-01", "2019-12-01"))
+
+  expect_identical(lag_periode(dato_lt_med_klokke, 5), lag_periode(dato_Date, 5))
+  expect_true(any(lag_periode(dato_lt_med_klokke_diff, 500) != lag_periode(dato_Date, 500)))
+})
+
 
 
 context("Funksjon 2 - Utdata")
 
+# Utdata samsvarer med forventet resultat
+test_that("Utdata samsvarer med forventet resultat, ", {
+  dato = as.Date(c("2019-01-01", "2019-04-01", "2019-08-01", "2019-12-01"))
+  forvent = c(2019.125, 2019.125, 2019.625, 2019.875)
+  forvent2 = c(2019.25, 2019.25, 2019.75, 2019.75)
+  expect_identical(lag_periode(dato, 4), forvent)
+  expect_identical(lag_periode(dato, 2), forvent2)
+})
+
 # Utdata er aldri heltall, selv med flere enn 365 deler.
 test_that("Utdata aldri er et heltall", {
+  dato_diger = sample(seq(from = as.Date("2019-01-01"), to = as.Date("2025-12-31"), "days"), size = 3000, replace = TRUE)
+  dato_aarevis = sample(seq(from = as.Date("2019-01-01"), to = as.Date("2025-12-31"), "days"), size = 30, replace = TRUE)
   expect_false(any(lag_periode(dato_diger, 10000) == floor(lag_periode(dato_diger, 10000)))) &
     expect_false(any(lag_periode(dato_diger, 3) == floor(lag_periode(dato_diger, 3)))) &
-    expect_false(any(lag_periode(dato_arevis, 1000) == floor(lag_periode(dato_arevis, 1000))))
+    expect_false(any(lag_periode(dato_aarevis, 1000) == floor(lag_periode(dato_aarevis, 1000))))
 })
 
 # Utdata er alltid et tall innen riktig år sammenlignet med dato.
 test_that("Utdata aldri er et heltall", {
+  dato_diger = sample(seq(from = as.Date("2019-01-01"), to = as.Date("2025-12-31"), "days"), size = 3000, replace = TRUE)
   expect_identical(floor(lag_periode(dato_diger, 10000)), year(dato_diger))
+})
+
+# Symmetri i utdata rundt årsskifte.
+test_that("Avstand mellom ut-verdier er symmetrisk fordelt", {
+  dato = as.Date(c("2019-01-01", "2019-12-31"))
+  expect_identical((lag_periode(dato, 365)[1] - year(dato)[1]), ((year(dato)[1] + 1 - lag_periode(dato, 365)[2])))
+  expect_identical((lag_periode(dato, 5)[1] - year(dato)[1]), ((year(dato)[1] + 1 - lag_periode(dato, 5)[2])))
+  expect_identical((lag_periode(dato, 13)[1] - year(dato)[1]), ((year(dato)[1] + 1 - lag_periode(dato, 13)[2])))
+})
+
+context("Funksjon 2 - Grensetilfeller")
+
+# Få observasjoner i årstall
+test_that("Funksjonen fungerer med kun en datoverdi", {
+  dato = as.Date("2019-01-01")
+  expect_equal(lag_periode(dato, 4), 2019.125)
+})
+
+# Funksjonen fungerer med kun 1 del.
+test_that("Funksjonen fungerer med kun en del", {
+  dato = as.Date(c("2019-01-01", "2019-04-01", "2019-08-01", "2019-12-01"))
+  expect_equal(lag_periode(dato, 1), c(2019.5, 2019.5, 2019.5, 2019.5))
 })
