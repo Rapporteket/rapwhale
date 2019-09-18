0ea95647aafc975fc5946a0d4b17fab7a161639e
Lena Kristin Bache-Mathiesen <lenakba@users.noreply.github.com>
2019-09-18 08:02:30 +0000

    les_dd_oqr håndterer nå verdier med hermetegn rundt seg (ettersom OpenQReg-verdier kan ha dette rundt datoer)
    SVN-revisjon: 4669

--- felles/pakker/rapwhale/R/dformat-oqr.R
+++ felles/pakker/rapwhale/R/dformat-oqr.R
@@ -452,7 +452,7 @@ les_dd_oqr = function(mappe_dd, reg_id, skjema_id, status = 1, dato = NULL, kode
     tz = "Europe/Oslo"
   ) # Vert brukt både her og seinare
   d = readr::read_delim(adresse_dd,
-    delim = ";", quote = "", trim_ws = FALSE, na = "",
+    delim = ";", quote = "\"", trim_ws = FALSE, na = "",
     escape_double = FALSE, quoted_na = TRUE,
     col_names = spek_innlesing$variabel_id, col_types = kol_typar,
     skip = 1, # Hopp over overskriftsrada
