7eedd6eb05838c1679627fa4f25c5851aa828012
Endre Kvåle Evjen <endre.kvale.evjen@helse-bergen.no>
2019-10-24 09:30:42 +0000

    lagt inn t.test for konfidensintervall
    SVN-revisjon: 4953

--- felles/pakker/rapwhale/R/regn_ki_snitt.R
+++ felles/pakker/rapwhale/R/regn_ki_snitt.R
@@ -1,5 +1,4 @@
 
-
 aggreger_ki_snitt = function(d_ki_ind, alfa = 0.05) {
   # Teste inndata
   if (!(is.data.frame(d_ki_ind) && all(hasName(d_ki_ind, c("ki_x", "ki_aktuell"))))) {
@@ -14,4 +13,13 @@ aggreger_ki_snitt = function(d_ki_ind, alfa = 0.05) {
   if (any(d_ki_ind$ki_aktuell == TRUE & is.na(d_ki_ind$ki_x))) {
     stop("'ki_x' må være en numerisk verdi hvis 'ki_aktuell' er TRUE")
   }
+
+  konf = t.test(x = d_ki_ind$ki_x, conf.level = 1 - alfa)
+  d_ki_ind %>%
+    summarise(
+      est = mean(ki_x, na.rm = TRUE),
+      konfint_nedre = as.numeric(unlist(konf)["conf.int1"]),
+      konfint_ovre = as.numeric(unlist(konf)["conf.int2"]),
+      n_aktuell = sum(ki_aktuell)
+    )
 }
