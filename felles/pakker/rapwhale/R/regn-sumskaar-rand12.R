82a80264d9f516c8e3b9b9a192e1652caf2dd6d4
Stian Tufte Veisene <stian.tufte.veisene@helse-bergen.no>
2019-12-05 15:16:03 +0000

    Fikset RAND-12-skåringsfunksjon til å brukte riktige variabler
    SVN-revisjon: 5055

--- felles/pakker/rapwhale/R/regn-sumskaar-rand12.R
+++ felles/pakker/rapwhale/R/regn-sumskaar-rand12.R
@@ -5,8 +5,8 @@
 skaar_rand12 = function(d, variabelnavn, metode = "farivar_2007_oblique") {
   stopifnot(metode = "farivar_2007_oblique")
   skaaringstabell = tibble(variabel = "variabel", verdi = "verdi", koeffisient = "koeffisient")
-  d = endre_variabelnavn(d, variabelnavn)
-  sjekk_variabelnavn(d, variabelnavn = skaaringstabell$variabel)
+  d = endre_variabelnavn(d, skaaringstabell$variabel)
+  sjekk_variabelnavn(d, variabelnavn)
   sjekk_variabelverdier(d, verditabell = skaaringstabell %>% select(variabel, verdi), godta_manglende)
   regn_sumskaar(d, skaaringstabell)
 }
