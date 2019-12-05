2118ecf7302c4067776594bd0611c63e1c1b6f2f
Stian Tufte Veisene <stian.tufte.veisene@helse-bergen.no>
2019-12-05 15:15:18 +0000

    Implementert sjekk_variabelverdier()
    Funksjonen fungerer (oppdager feil), men må forbedres til
    blant annet å gi informasjon om hvilke variabler som har
    hvilke feil.
    
    SVN-revisjon: 5054

--- felles/pakker/rapwhale/R/regn-sumskaar-generelt.R
+++ felles/pakker/rapwhale/R/regn-sumskaar-generelt.R
@@ -17,11 +17,31 @@ sjekk_variabelnavn = function(d, variabelnavn) {
   }
 }
 
-sjekk_variabelverdier = function(d,
-                                 verditabell = skaaringstabell %>%
-                                   select(variabel, verdi),
-                                 godta_manglende) {
+sjekk_variabelverdier = function(d, koblingstabell, godta_manglende) {
+  if (!(is.data.frame(koblingstabell) && all(hasName(koblingstabell, c("variabel", "verdi"))))) {
+    stop("Inndata må være tibble/data.frame og inneholde kolonnene 'variabel' og 'verdi'")
+  }
 
+  # variabelnavn_akt = names(d)[names(d)%in% koblingstabell$variabel]
+  alle_ugyldige = vector()
+  for (i in names(d)) {
+    verdier_d = d[[i]]
+    verdier_koblingstabell = koblingstabell %>%
+      filter(variabel == !!i) %>%
+      pull(verdi)
+
+    if (godta_manglende) {
+      verdier_koblingstabell = append(verdier_koblingstabell, NA)
+    }
+    verdier_d_ugyldig = setdiff(verdier_d, verdier_koblingstabell)
+    if (length(verdier_d_ugyldig) > 0) {
+      alle_ugyldige = append(alle_ugyldige, verdier_d_ugyldig)
+    }
+  }
+  alle_ugyldige = unique(alle_ugyldige)
+  if (length(alle_ugyldige) > 0) {
+    stop("Ugyldige verdier: ", paste0(alle_ugyldige, collapse = ", "))
+  }
 }
 
 regn_sumskaar = function(d, skaaringstabell) {
