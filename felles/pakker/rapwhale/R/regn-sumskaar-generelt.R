fce5653943e4097416c1260c95d13f09cfa6f669
Stian Tufte Veisene <stian.tufte.veisene@helse-bergen.no>
2019-12-17 14:22:32 +0000

    Lagt til funksjonen finn_ugyldige_verdier() som skal returnere en dataramme med radnummer, variabelnavn og feilverdier
    SVN-revisjon: 5080

--- felles/pakker/rapwhale/R/regn-sumskaar-generelt.R
+++ felles/pakker/rapwhale/R/regn-sumskaar-generelt.R
@@ -1,13 +1,6 @@
 # Generelle funksjoner for utregning av sumskårer
 
-
-# Spesifikke funksjoner
-
-endre_variabelnavn = function(d, variabelnavn) {
-
-}
-
-# Skal sjekke at variabelnavnene i d er identiske til variabelnavnene i skaaringstabell
+# Funksjon som sjekker at variabelnavnene i d er identiske til variabelnavnene i skaaringstabell
 
 sjekk_variabelnavn = function(d, variabelnavn) {
   var_mangler = unique(variabelnavn[!(variabelnavn %in% names(d))])
@@ -16,6 +9,38 @@ sjekk_variabelnavn = function(d, variabelnavn) {
     stop("Mangler kolonner: ", var_mangler_tekst)
   }
 }
+
+# Funksjon som returnerer en dataramme med radnummer, variabelnavn og feilverdi for ugyldige verdier
+
+finn_ugyldige_verdier = function(d, koblingstabell) {
+  radnr_ugyldige = vector()
+  variabler_ugyldige = vector()
+  verdier_ugyldige = vector()
+  for (var_d in names(d)) {
+    verdier_d = d[[var_d]]
+    verdier_koblingstabell = koblingstabell %>%
+      filter(variabel == !!var_d) %>%
+      pull(verdi)
+    verdier_d_ugyldige = setdiff(verdier_d, verdier_koblingstabell)
+    if (length(verdier_d_ugyldige) > 0) {
+      radnr_ugyldige = append(radnr_ugyldige, which(verdier_d %in% verdier_d_ugyldige))
+      variabler_ugyldige = append(variabler_ugyldige, rep(var_d, times = length(verdier_d_ugyldige)))
+      verdier_ugyldige = append(verdier_ugyldige, verdier_d_ugyldige)
+    }
+  }
+  oversikt_ugyldige = arrange(
+    tibble(
+      radnr = as.integer(radnr_ugyldige),
+      variabel = as.character(variabler_ugyldige),
+      feilverdi = as.numeric(verdier_ugyldige)
+    ),
+    radnr
+  )
+  oversikt_ugyldige
+}
+
+
+################################# Midlertidig kode ################################
 
 sjekk_variabelverdier = function(d, koblingstabell, godta_manglende) {
   if (!(is.data.frame(koblingstabell) && all(hasName(koblingstabell, c("variabel", "verdi"))))) {
@@ -42,6 +67,10 @@ sjekk_variabelverdier = function(d, koblingstabell, godta_manglende) {
   if (length(alle_ugyldige) > 0) {
     stop("Ugyldige verdier: ", paste0(alle_ugyldige, collapse = ", "))
   }
+}
+
+endre_variabelnavn = function(d, variabelnavn) {
+
 }
 
 regn_sumskaar = function(d, skaaringstabell) {
