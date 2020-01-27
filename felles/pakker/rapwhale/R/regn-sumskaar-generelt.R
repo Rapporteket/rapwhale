f35236373622dd42156ca1b4825faa4bdd5f9ca9
Stian Tufte Veisene <stian.tufte.veisene@helse-bergen.no>
2020-01-27 13:54:07 +0000

    Lagt til en overordnet funksjon. Endret slik at sjekk_variabelverdier() kaller på finn_ugyldige_verdier() og oppsummer_ugyldige_verdier(). Utbedret finn_ugyldige_verdier() og dokumentert den
    SVN-revisjon: 5108

--- felles/pakker/rapwhale/R/regn-sumskaar-generelt.R
+++ felles/pakker/rapwhale/R/regn-sumskaar-generelt.R
@@ -1,7 +1,19 @@
 # Generelle funksjoner for utregning av sumskårer
 
-# Funksjon som sjekker at variabelnavnene i d er identiske til variabelnavnene i skaaringstabell
+# Overordnet funksjon
+skaar = function(d, variabelnavn, skaaringstabell) {
+  d = endre_variabelnavn(d, variabelnavn)
+  sjekk_variabelnavn(d, variabelnavn = skaaringstabell$variabel)
+  sjekk_variabelverdier(d, verditabell = select(skaaringstabell, variabel, verdi))
+  regn_sumskaar(d, skaaringstabell)
+}
 
+# Endre variabelnavn
+endre_variabelnavn = function(d, variabelnavn) {
+
+}
+
+# Funksjon som sjekker at variabelnavnene i d er identiske til variabelnavnene i skaaringstabell
 sjekk_variabelnavn = function(d, variabelnavn) {
   var_mangler = unique(variabelnavn[!(variabelnavn %in% names(d))])
   var_mangler_tekst = paste0(var_mangler, collapse = ", ")
@@ -10,22 +22,42 @@ sjekk_variabelnavn = function(d, variabelnavn) {
   }
 }
 
-# Funksjon som returnerer en dataramme med radnummer, variabelnavn og feilverdi for ugyldige verdier
+sjekk_variabelverdier = function(d, verditabell, godta_manglende) {
+  if (!(is.data.frame(verditabell) &&
+    all(hasName(verditabell, c("variabel", "verdi"))))) {
+    stop("Inndata må være tibble/data.frame og inneholde kolonnene 'variabel' og 'verdi'")
+  }
+  d_ugyldige_verdier = finn_ugyldige_verdier(d, verditabell, godta_manglende)
+  if (nrow(d_ugyldige_verdier) > 0) {
+    oppsummering = oppsummer_ugyldige_verdier(d_ugyldige_verdier)
+    stop(oppsummering)
+  }
+}
 
-finn_ugyldige_verdier = function(d, koblingstabell) {
-  radnr_ugyldige = vector()
-  variabler_ugyldige = vector()
-  verdier_ugyldige = vector()
+#'
+#' @param d Dataramme/tibble med nøyaktig samme variabelnavn som finnes i variabel-kolonnen i verditabell.
+#' @param verditabell Dataramme/tibble med kolonne variabel og verdi, som sier hvilke verdier som er gyldige
+#'     for hvilke variabler.
+#' @param godta_manglende Om NA-verdier skal regnes som gydlige (selv om de ikke er nevnt i verditabell).
+#'
+#' @return Dataramme med radnummer, variabelnavn og feilverdi for ugyldige verdier. Sortert etter radnummer
+#'     og så rekkefølge i d.
+
+finn_ugyldige_verdier = function(d, verditabell, godta_manglende) {
+  radnr_ugyldige = integer()
+  variabler_ugyldige = character()
+  verdier_ugyldige = numeric()
   for (var_d in names(d)) {
     verdier_d = d[[var_d]]
-    verdier_koblingstabell = koblingstabell %>%
+    mulige_verdier = verditabell %>%
       filter(variabel == !!var_d) %>%
       pull(verdi)
-    verdier_d_ugyldige = setdiff(verdier_d, verdier_koblingstabell)
+    verdier_d_ugyldige = setdiff(verdier_d, mulige_verdier)
     if (length(verdier_d_ugyldige) > 0) {
-      radnr_ugyldige = append(radnr_ugyldige, which(verdier_d %in% verdier_d_ugyldige))
-      variabler_ugyldige = append(variabler_ugyldige, rep(var_d, times = length(verdier_d_ugyldige)))
-      verdier_ugyldige = append(verdier_ugyldige, verdier_d_ugyldige)
+      indeks_ugyldige = which(verdier_d %in% verdier_d_ugyldige)
+      radnr_ugyldige = append(radnr_ugyldige, indeks_ugyldige)
+      variabler_ugyldige = append(variabler_ugyldige, rep(var_d, times = length(indeks_ugyldige)))
+      verdier_ugyldige = append(verdier_ugyldige, verdier_d[indeks_ugyldige])
     }
   }
   oversikt_ugyldige = arrange(
@@ -49,37 +81,35 @@ oppsummer_ugyldige_verdier = function(d_ugyldige) {
 
 ################################# Midlertidig kode ################################
 
-sjekk_variabelverdier = function(d, koblingstabell, godta_manglende) {
-  if (!(is.data.frame(koblingstabell) && all(hasName(koblingstabell, c("variabel", "verdi"))))) {
-    stop("Inndata må være tibble/data.frame og inneholde kolonnene 'variabel' og 'verdi'")
-  }
-
-  # variabelnavn_akt = names(d)[names(d)%in% koblingstabell$variabel]
-  alle_ugyldige = vector()
-  for (var_d in names(d)) {
-    verdier_d = d[[var_d]]
-    verdier_koblingstabell = koblingstabell %>%
-      filter(variabel == !!var_d) %>%
-      pull(verdi)
-
-    if (godta_manglende) {
-      verdier_koblingstabell = append(verdier_koblingstabell, NA)
-    }
-    verdier_d_ugyldige = setdiff(verdier_d, verdier_koblingstabell)
-    if (length(verdier_d_ugyldige) > 0) {
-      alle_ugyldige = append(alle_ugyldige, paste0(var_d, ": ", paste0(verdier_d_ugyldige, collapse = ", ")))
-    }
-  }
-  alle_ugyldige = unique(alle_ugyldige)
-  if (length(alle_ugyldige) > 0) {
-    stop("Ugyldige verdier: ", paste0(alle_ugyldige, collapse = ", "))
-  }
-}
-
-endre_variabelnavn = function(d, variabelnavn) {
-
-}
-
-regn_sumskaar = function(d, skaaringstabell) {
-
-}
+# midl_funk = function(d, koblingstabell, godta_manglende) {
+#   if (!(is.data.frame(koblingstabell) && all(hasName(koblingstabell, c("variabel", "verdi"))))) {
+#     stop("Inndata må være tibble/data.frame og inneholde kolonnene 'variabel' og 'verdi'")
+#   }
+#
+#   # variabelnavn_akt = names(d)[names(d)%in% koblingstabell$variabel]
+#   alle_ugyldige = vector()
+#   for (var_d in names(d)) {
+#     verdier_d = d[[var_d]]
+#     verdier_koblingstabell = koblingstabell %>%
+#       filter(variabel == !!var_d) %>%
+#       pull(verdi)
+#
+#     if (godta_manglende) {
+#       verdier_koblingstabell = append(verdier_koblingstabell, NA)
+#     }
+#     verdier_d_ugyldige = setdiff(verdier_d, verdier_koblingstabell)
+#     if (length(verdier_d_ugyldige) > 0) {
+#       alle_ugyldige = append(alle_ugyldige, paste0(var_d, ": ", paste0(verdier_d_ugyldige, collapse = ", ")))
+#     }
+#   }
+#   alle_ugyldige = unique(alle_ugyldige)
+#   if(length(alle_ugyldige) > 0) {
+#     stop("Ugyldige verdier: ", paste0(alle_ugyldige, collapse = ", "))
+#   }
+# }
+#
+#
+#
+# regn_sumskaar = function(d, skaaringstabell) {
+#
+# }
