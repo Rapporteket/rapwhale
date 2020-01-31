5b75d313867d5f6a160d4e112a088e75d81c571a
Endre Kvåle Evjen <endre.kvale.evjen@helse-bergen.no>
2020-01-31 12:39:09 +0000

    Lagt inn funksjon konverter_boolske og oqr_boolsk_til_boolsk
    SVN-revisjon: 5125

--- felles/pakker/rapwhale/R/les_dd_oqr_csv.R
+++ felles/pakker/rapwhale/R/les_dd_oqr_csv.R
@@ -30,3 +30,23 @@ sjekk_varnavn = function(dd_sti, varnavn_kilde) {
     warning("Variabelnavn har ulik rekkefølge i datadump og spesifikasjon")
   }
 }
+
+# Håndtering av spesielle tilfeller, variabeltype = boolsk og dato_kl
+oqr_boolsk_til_boolsk = function(x) {
+
+  # Sjekk først at det berre er gyldige verdiar
+  er_gyldig = (x %in% c("0", "1")) | is.na(x)
+  if (!all(er_gyldig)) {
+    stop(error = "Det finnes ugyldige verdier for en boolsk variabel. (Må være 0,1 eller NA)")
+  } else {
+    x == 1 # Gjer om til boolsk variabel
+  }
+}
+
+# Funksjon som konverterer boolske variabler til "ekte" boolske.
+konverter_boolske = function(d, vartype) {
+  ind = which(vartype == "boolsk")
+  d = d %>%
+    mutate_at(ind, oqr_boolsk_til_boolsk)
+  d
+}
