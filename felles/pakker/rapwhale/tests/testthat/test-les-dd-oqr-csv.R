41139c1280aba3dbf1a0b81a9c019b7cd00d8c57
Endre Kvåle Evjen <endre.kvale.evjen@helse-bergen.no>
2020-01-31 13:49:21 +0000

    Oppdatert stier for testing av .csv-filer
    SVN-revisjon: 5132

--- felles/pakker/rapwhale/tests/testthat/test-les-dd-oqr-csv.R
+++ felles/pakker/rapwhale/tests/testthat/test-les-dd-oqr-csv.R
@@ -1,71 +1,73 @@
-# csv-filer for testing ligger i mappen:
-mappe = "tests/testthat/"
-
 # Håndtering av inndata fra datadump og spesifikasjon ---------------------
 context("les_varnavn")
 
 test_that("Funksjonen leser inn en variabel selv om den ikke har navn", {
-  dd_kolonne_uten_navn = "dd_kolonne_uten_navn.csv"
   forventet_uten_navn = c("alfa", "beta", "", "delta", "echo")
-  expect_identical(les_varnavn(dd_sti = paste0(mappe, dd_kolonne_uten_navn)), forventet_uten_navn)
+  expect_identical(les_varnavn(dd_sti = "dd_kolonne_uten_navn.csv"), forventet_uten_navn)
 })
 
 test_that("Funksjonen gir feilmelding om to variabler har samme navn i datadump", {
-  dd_to_kolonner_med_samme_navn = "dd_to_kolonner_med_samme_navn.csv"
   forventet_med_samme_navn = "Variabler i datadump kan ikke ha samme navn"
 
-  expect_error(les_varnavn(dd_sti = paste0(mappe, dd_to_kolonner_med_samme_navn)), forventet_med_samme_navn)
+  expect_error(les_varnavn(dd_sti = "dd_to_kolonner_med_samme_navn.csv"), forventet_med_samme_navn)
 })
 
 test_that("Funksjonen leser inn variabler med mellomrom i navnet", {
-  dd_kolonnenavn_med_mellomrom = "dd_kolonnenavn_med_mellomrom.csv"
   forventet_med_mellomrom = c("alfa", "beta", "beta bert", "delta", "echo")
 
-  expect_identical(les_varnavn(dd_sti = paste0(mappe, dd_kolonnenavn_med_mellomrom)), forventet_med_mellomrom)
+  expect_identical(les_varnavn(dd_sti = "dd_kolonnenavn_med_mellomrom.csv"), forventet_med_mellomrom)
 })
 
 test_that("Funksjonen leser inn variabler hvor navnet starter med et tall", {
-  dd_kolonnenavn_som_starter_med_tall = "dd_kolonnenavn_som_starter_med_tall.csv"
   forventet_med_tall = c("alfa", "beta", "2beta", "delta", "echo")
 
-  expect_identical(les_varnavn(dd_sti = paste0(mappe, dd_kolonnenavn_som_starter_med_tall)), forventet_med_tall)
+  expect_identical(les_varnavn(dd_sti = "dd_kolonnenavn_som_starter_med_tall.csv"), forventet_med_tall)
 })
 
 test_that("Funksjonen leser inn variabler med bindestrek i navnet", {
-  dd_kolonnenavn_med_bindestrek = "dd_kolonnenavn_med_bindestrek.csv"
   forventet_med_bindestrek = c("alfa", "beta", "beta-bert", "delta", "echo")
 
-  expect_identical(les_varnavn(dd_sti = paste0(mappe, dd_kolonnenavn_med_bindestrek)), forventet_med_bindestrek)
+  expect_identical(les_varnavn(dd_sti = "dd_kolonnenavn_med_bindestrek.csv"), forventet_med_bindestrek)
 })
 
 context("sjekk_varnavn")
 
-dd_ok = "dd_ok.csv"
 varnavn_ok = c("alfa", "beta", "cappa", "delta", "echo")
 varnavn_feil_rekkefolge = c("alfa", "beta", "delta", "cappa", "echo")
 varnavn_feil_navn = c("alfa", "beta", "cappa", "delta", "eple")
 
 # Feilmelding hvis det ikke er overenstemmelse
 test_that("Funksjonen gir feilmelding hvis variabelnavn i datadump ikke stemmer overens med navn i spesifikasjon", {
-  expect_identical(sjekk_varnavn(paste0(mappe, dd_ok), varnavn_ok), NULL) # Forventer null return
-  expect_warning(sjekk_varnavn(paste0(mappe, dd_ok), varnavn_feil_rekkefolge), "Variabelnavn har ulik rekkefølge i datadump og spesifikasjon") # Ulik rekkefølge i spes sammenlignet med dd.
-  expect_error(sjekk_varnavn(paste0(mappe, dd_ok), varnavn_feil_navn), "Variabelnavn i spesifikasjon stemmer ikke overens med variabelnavn i datadump") # Feil navn
+  expect_identical(sjekk_varnavn("dd_ok.csv", varnavn_ok), NULL) # Forventer null return
+  expect_warning(sjekk_varnavn("dd_ok.csv", varnavn_feil_rekkefolge), "Variabelnavn har ulik rekkefølge i datadump og spesifikasjon") # Ulik rekkefølge i spes sammenlignet med dd.
+  expect_error(sjekk_varnavn("dd_ok.csv", varnavn_feil_navn), "Variabelnavn i spesifikasjon stemmer ikke overens med variabelnavn i datadump") # Feil navn
 })
-
 
 # Konvertering av variabeltyper -------------------------------------------
 context("konverter_boolske")
 
+oqr_specs_dd_ok_hel = tibble::tribble(
+  ~navn_kilde, ~nye_varnavn, ~vartype, ~csv_bokstav,
+  "alfa", "Abso", "kategorisk", "c",
+  "beta", "Beto", "tekst", "c",
+  "gamma", "Gammo", "numerisk", "d",
+  "delta", "Delto", "heltall", "i",
+  "epsilon", "Eple", "boolsk", "c",
+  "zeta", "Zeppelin", "dato_kl", "c",
+  "eta", "Estland", "dato", "D",
+  "theta", NA_character_, "kl", "t"
+)
+
 d_base = les_oqr_csv_base(
-  dd_sti = paste0(mappe, dd_ok_hel),
+  dd_sti = "dd_ok_hel.csv",
   varnavn_kilde = oqr_specs_dd_ok_hel$navn_kilde,
-  nye_varnavn = nye_navn_mangler,
+  nye_varnavn = oqr_specs_dd_ok_hel$nye_varnavn,
   vartype = oqr_specs_dd_ok_hel$vartype,
   csv_bokstav = oqr_specs_dd_ok_hel$csv_bokstav
-) # Fixme - Flytte denne opp
+)
 
 d_base_m_feil_logisk = d_base
-d_base_m_feil_logisk$epsilon[2] = "2"
+d_base_m_feil_logisk$Eple[2] = "2"
 
 test_that("Funksjonen gir feilmelding hvis en boolsk variabel inneholder andre verdier enn (0,1,NA)", {
   expect_error(konverter_boolske(d = d_base_m_feil_logisk, vartype = oqr_specs_dd_ok_hel$vartype),
@@ -76,12 +78,12 @@ test_that("Funksjonen gir feilmelding hvis en boolsk variabel inneholder andre v
 
 test_that("Funksjonen klarer å konvertere flere boolske", {
   d_base_m_flere_logisk = d_base
-  ekstra_logisk = d_base$epsilon
+  ekstra_logisk = d_base$Eple
   d_base_m_flere_logisk = bind_cols(d_base_m_flere_logisk, ekstra_logisk = ekstra_logisk)
 
   forventet_m_ekstra_logisk = d_base_m_flere_logisk
-  forventet_m_ekstra_logisk$epsilon = as.logical(d_base_m_flere_logisk$epsilon)
-  forventet_m_ekstra_logisk$epsilon = c(TRUE, FALSE, NA, TRUE)
+  forventet_m_ekstra_logisk$Eple = as.logical(d_base_m_flere_logisk$Eple)
+  forventet_m_ekstra_logisk$Eple = c(TRUE, FALSE, NA, TRUE)
   forventet_m_ekstra_logisk$ekstra_logisk = as.logical(d_base_m_flere_logisk$ekstra_logisk)
   forventet_m_ekstra_logisk$ekstra_logisk = c(TRUE, FALSE, NA, TRUE)
 
