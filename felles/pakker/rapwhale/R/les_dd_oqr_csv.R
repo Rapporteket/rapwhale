e1034989c05efeef208d52f34967ccb50a488b6a
Endre Kvåle Evjen <endre.kvale.evjen@helse-bergen.no>
2020-02-12 14:00:00 +0000

    Fikset funksjon for å konvertere boolske variabler
    SVN-revisjon: 5196

--- felles/pakker/rapwhale/R/les_dd_oqr_csv.R
+++ felles/pakker/rapwhale/R/les_dd_oqr_csv.R
@@ -17,39 +17,22 @@ les_varnavn = function(adresse, formatspek) {
 }
 
 # Håndtering av spesielle tilfeller, variabeltype = boolsk og dato_kl
-oqr_boolsk_til_boolsk = function(x) {
+konverter_boolske = function(x, boolsk_usann, boolsk_sann) {
 
   # Sjekk først at det berre er gyldige verdiar
-  er_gyldig = (x %in% c("0", "1")) | is.na(x)
-  if (!all(er_gyldig)) {
-    stop(error = "Det finnes ugyldige verdier for en boolsk variabel. (Må være 0,1 eller NA)")
-  } else {
-    x == 1 # Gjer om til boolsk variabel
+  er_gyldig = (x %in% c(boolsk_sann, boolsk_usann)) | is.na(x)
+  if (!rlang::is_empty(intersect(boolsk_sann, boolsk_usann))) {
+    stop(error = paste0("boolsk_sann og boolsk_usann kan ikke inneholde samme verdier."))
   }
+  if (!all(er_gyldig)) {
+    stop(error = paste0("Det finnes ugyldige verdier for en boolsk variabel.\nboolsk_usann kan være: ", boolsk_usann, "\nboolsk_sann kan være: ", boolsk_sann))
+  }
+  x == boolsk_sann # Gjer om til boolsk variabel
+  x_boolsk = rep(NA, length(x))
+  x_boolsk[x %in% boolsk_usann] = FALSE
+  x_boolsk[x %in% boolsk_sann] = TRUE
+  x_boolsk
 }
-
-# Funksjon som konverterer boolske variabler til "ekte" boolske.
-konverter_boolske = function(d, vartype) {
-  ind = which(vartype == "boolsk")
-  d = d %>%
-    mutate_at(ind, oqr_boolsk_til_boolsk)
-  d
-}
-
-# Funksjon som konverterer dato_kl variabler fra character til DD.MM.YYYY HH:MM format.
-konverter_dato_kl = function(d, vartype) { # fixme - Hvordan skal den reagere på dato_kl i feil format?
-  ind = which(vartype == "dato_kl")
-  d = d %>%
-    mutate_at(ind, readr::parse_datetime,
-      format = "%d.%m.%Y %H:%M",
-      locale = locale(
-        decimal_mark = ",",
-        tz = "Europe/Oslo"
-      )
-    )
-  d
-}
-
 
 #' Konverter variabeltype fra standard format til readrformat
 #'
@@ -71,7 +54,7 @@ std_koltype_til_readr_koltype = function(vartype) {
     "tekst", "c",
     "desimaltall", "d",
     "heltall", "i",
-    "boolsk", "l",
+    "boolsk", "c",
     "dato", "D",
     "dato_kl", "c",
     "kl", "t"
@@ -133,12 +116,6 @@ les_csv_base = function(adresse, spesifikasjon, formatspek) {
     boolsk_usann = formatspek$boolsk_usann,
     boolsk_sann = formatspek$boolsk_sann
   )
-  # Konverter_boolsk:
-  sjekk(is_empty(intersect(boolsk_usann, boolsk_sann)))
-  x[x %in% boolsk_usann] = FALSE
-  x[x %in% boolsk_sann] = TRUE
-
-  # Konvertering og fiksing av diverse variabeltyper
 
   d
 }
