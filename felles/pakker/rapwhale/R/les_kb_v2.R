93904403316a9c867dbff3e184e307d948466cf1
Endre Kvåle Evjen <endre.kvale.evjen@helse-bergen.no>
2020-05-05 13:20:03 +0000

    valider_oqr_kb - La inn sortering av rekkefølge for skjema i kodebok
    SVN-revisjon: 5481

--- felles/pakker/rapwhale/R/les_kb_v2.R
+++ felles/pakker/rapwhale/R/les_kb_v2.R
@@ -233,6 +233,10 @@ valider_oqr_kb = function(kb_mellom) {
   kb_std = velg_standardkolonner(kb_mellom)
   kb_std = fiks_skjemanavn(kb_std)
 
+  # Ordne rekkefølge for variabler slik variabler fra samme tabell kommer samlet
+  kb_std = kb_std %>%
+    arrange(forcats::fct_inorder(skjema_id))
+
   kb_std
 }
 
