3c69dda97f344d0345ccdd7156e69719287e7685
Endre Kvåle Evjen <endre.kvale.evjen@helse-bergen.no>
2020-06-03 08:47:11 +0000

    Lagt inn kall til pakkene tibble og dplyr for de funksjonene som hentes fra de pakkene
    SVN-revisjon: 5565

--- felles/pakker/rapwhale/R/regn-sumskaar-rand12.R
+++ felles/pakker/rapwhale/R/regn-sumskaar-rand12.R
@@ -4,9 +4,9 @@
 
 skaar_rand12 = function(d, variabelnavn, metode = "farivar_2007_oblique") {
   stopifnot(metode = "farivar_2007_oblique")
-  skaaringstabell = tibble(variabel = "variabel", verdi = "verdi", koeffisient = "koeffisient")
+  skaaringstabell = tibble::tibble(variabel = "variabel", verdi = "verdi", koeffisient = "koeffisient")
   d = endre_variabelnavn(d, skaaringstabell$variabel)
   sjekk_variabelnavn(d, variabelnavn)
-  sjekk_variabelverdier(d, verditabell = skaaringstabell %>% select(variabel, verdi), godta_manglende)
+  sjekk_variabelverdier(d, verditabell = skaaringstabell %>% dplyr::select(variabel, verdi), godta_manglende)
   regn_sumskaar(d, skaaringstabell)
 }
