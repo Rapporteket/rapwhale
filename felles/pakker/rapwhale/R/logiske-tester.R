ca721b4bfc73e4cb69e38a583e63299257dc07f0
Stian Tufte Veisene <stian.tufte.veisene@helse-bergen.no>
2020-10-13 12:50:38 +0000

    Lagt til første utkast til dokumentasjon for de to funksjonene
    SVN-revisjon: 6064

--- felles/pakker/rapwhale/R/logiske-tester.R
+++ felles/pakker/rapwhale/R/logiske-tester.R
@@ -1,13 +1,33 @@
 # Generelle funksjoner for å teste logikker
 
-# Sann hvis og bare hvis 'a' sann impliserer 'b' sann
-# (og håndterer NA-verdier fint, og gir alltid ut TRUE eller FALSE,
-# aldri NA)
+#' Sjekk om 'a' sann impliserer 'b' sann
+#'
+#' @description
+#' Sann hvis og bare hvis 'a' sann impliserer 'b' sann
+#' (og håndterer NA-verdier fint, og gir alltid ut TRUE eller FALSE,
+#' aldri NA)
+#'
+#' @param a
+#' @param b
+#'
+#' @return TRUE eller FALSE
+#' @export
 impl = function(a, b) {
   (is.na(a) | !a) | tidyr::replace_na(b, FALSE) # eg. (!a | b), men håndterer NA
 }
 
-# Hvis og bare hvis (som håndterer NA)
+#' Sjekk om 'a' sann impliserer 'b' sann, og omvendt
+#'
+#' @description
+#' Sann hvis og bare hvis 'a' sann impliserer 'b' sann, og omvendt
+#' (og håndterer NA-verdier fint, og gir alltid ut TRUE eller FALSE,
+#' aldri NA)
+#'
+#' @param a
+#' @param b
+#'
+#' @return TRUE eller FALSE
+#' @export
 dobbelimpl = function(a, b) {
   impl(a, b) & impl(b, a)
 }
