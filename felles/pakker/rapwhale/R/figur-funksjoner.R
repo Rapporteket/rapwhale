965d901de18c4c163ff821f0fb0f59cce730fc64
Stian Tufte Veisene <stian.tufte.veisene@helse-bergen.no>
2020-10-26 09:05:39 +0000

    Lagt til funksjonene farger_kvalreg() og tema_kvalreg()
    Eksisterende kode tilknyttet fargeinnstillinger er lagt til farger_kvalreg() og kode tilknyttet tema-innstillinger er lagt til tema_kvalreg(). Objekter i tema_kvalreg() som ikke gjelder tema-innstillinger skal flyttes ut av funksjonen.
    
    SVN-revisjon: 6087

--- felles/pakker/rapwhale/R/figur-funksjoner.R
+++ felles/pakker/rapwhale/R/figur-funksjoner.R
@@ -7,72 +7,96 @@
 #' @importFrom lubridate is.Date
 #' @importFrom qicharts2 qic
 #' @import ggplot2
+NULL
+#' Farger for bruk i figurer
+#'
+#' @description
+#' Inneholder offisielle fargekoder som skal brukes i figurer
+#' i årsrapporter.
+#'
+#' @return En liste med `farger_hoved`, `farger_noyt` og `farger_kontr`.
+#' @export
+farger_kvalreg = function() {
 
+  # Dei offisielle fargene (som eg ikkje er så glad i)
+  # du mener, som INGEN liker.
+  colPrim = c("#000059", "#084594", "#2171b5", "#4292c6", "#6baed6", "#c6dbef") # Primærfarge (mørk til lys)
+  colNoyt = c("#4D4D4D", "#737373", "#A6A6A6", "#DADADA") # Nøytralfarge
+  colKontr = "#FF7260" # Kontrastfarge
 
-# Fargar og grafinnstillingar/-objekt -------------------------------------
-
-# Dei offisielle fargene (som eg ikkje er så glad i)
-# du mener, som INGEN liker.
-colPrim = c("#000059", "#084594", "#2171b5", "#4292c6", "#6baed6", "#c6dbef") # Primærfarge (mørk til lys)
-colNoyt = c("#4D4D4D", "#737373", "#A6A6A6", "#DADADA") # Nøytralfarge
-colKontr = "#FF7260" # Kontrastfarge
-
-# ggplot2-tema for figurar
-if (!exists("skriftstorleik")) { # Skriftstorleik bør vera definert i kvar årsrapportfil
-  skriftstorleik = 9
+  farger = list("farger_hoved" = colPrim, "farger_noyt" = colNoyt, "farger_kontr" = colKontr)
+  farger
 }
-tema = theme_light(base_size = skriftstorleik)
-tema$panel.grid.minor$colour = "white"
-tema$strip.background$fill = "#f3f1ee"
-tema$strip.background$colour = "#e4e0da"
-tema$strip.text.x = element_text(colour = "black")
-tema$panel.spacing = unit("13", "pt")
-tema$panel.border$colour = tema$strip.background$colour
-tema$panel.grid.major$colour = tema$strip.background$colour
-tema$panel.grid.minor$colour = tema$strip.background$fill
-tema$axis.title.y$angle = 0
-tema$axis.title.y$margin = margin(r = 5)
-tema$axis.title.x$margin = margin(t = 5)
 
-# Fjern vannrette eller loddrette rutenett
-fjern_x = theme(
-  panel.grid.major.x = element_blank(),
-  panel.grid.minor.x = element_blank()
-)
-fjern_y = theme(
-  panel.grid.major.y = element_blank(),
-  panel.grid.minor.y = element_blank()
-)
+#' Tema for bruk i figurer
+#'
+#' @description
+#' Inneholder temainnstillinger som skal brukes i figurer
+#' i årsrapporter.
+#'
+#' @return Objektet `tema` som inneholder temainnstillinger.
+#' @export
+tema_kvalreg = function() {
 
-# Fjern strekmarkeringar for viste tal/kategoriar
-# (tilsvarer «major breaks» på aksen).
-# Dette er nyttig for søylediagram med kategoriske
-# verdiar, der strekmarkeringane er unødvendige/stygge.
-fjern_x_ticks = theme(axis.ticks.x = element_blank())
-fjern_y_ticks = theme(axis.ticks.y = element_blank())
+  # ggplot2-tema for figurar
+  if (!exists("skriftstorleik")) { # Skriftstorleik bør vera definert i kvar årsrapportfil
+    skriftstorleik = 9
+  }
+  tema = theme_light(base_size = skriftstorleik)
+  tema$panel.grid.minor$colour = "white"
+  tema$strip.background$fill = "#f3f1ee"
+  tema$strip.background$colour = "#e4e0da"
+  tema$strip.text.x = element_text(colour = "black")
+  tema$panel.spacing = unit("13", "pt")
+  tema$panel.border$colour = tema$strip.background$colour
+  tema$panel.grid.major$colour = tema$strip.background$colour
+  tema$panel.grid.minor$colour = tema$strip.background$fill
+  tema$axis.title.y$angle = 0
+  tema$axis.title.y$margin = margin(r = 5)
+  tema$axis.title.x$margin = margin(t = 5)
 
-# Søyler skal i starta heilt inn til aksen, men ha litt luft
-# over seg, altså asymmetriske expand-verdiar. Her er ein
-# variabel som definerer dette, og som ein kan mata til
-# expand-argumentet til skaladefinisjonar.
-expand_soyle = expansion(mult = c(0.0, .05), add = 0)
+  # Fjern vannrette eller loddrette rutenett
+  fjern_x = theme(
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()
+  )
+  fjern_y = theme(
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank()
+  )
 
-# I noen tilfeller er det ikke tilstrekkelig plass for tekst-label i plot.
-# Da kan vi bruke expand-argumentet under.
-expand_soyle_str_fig = expansion(mult = c(0.0, .09), add = 0)
+  # Fjern strekmarkeringar for viste tal/kategoriar
+  # (tilsvarer «major breaks» på aksen).
+  # Dette er nyttig for søylediagram med kategoriske
+  # verdiar, der strekmarkeringane er unødvendige/stygge.
+  fjern_x_ticks = theme(axis.ticks.x = element_blank())
+  fjern_y_ticks = theme(axis.ticks.y = element_blank())
 
-# Fjern luft til venstre for y-akseteksten og legg
-# til ekstra luft til høgre for han, fjern luft under
-# x-akseteksten og legg til ekstra luft over han,
-# fjern nesten all luft rundt figurane (eventuell
-# nødvendig luft legg me til via LaTeX).
-#
-# (Merk: Me set den ytre margen til 3 punkt
-# opp og nede i staden for 0 punkt for å sikra at at
-# kantane på alle bokstavane alltid vert med.)
-tema$axis.title.y$margin = margin(r = tema$text$size / 2)
-tema$axis.title.x$margin = margin(t = tema$text$size / 2)
-tema$plot.margin = margin(3, 3, 3, 3)
+  # Søyler skal i starta heilt inn til aksen, men ha litt luft
+  # over seg, altså asymmetriske expand-verdiar. Her er ein
+  # variabel som definerer dette, og som ein kan mata til
+  # expand-argumentet til skaladefinisjonar.
+  expand_soyle = expansion(mult = c(0.0, .05), add = 0)
+
+  # I noen tilfeller er det ikke tilstrekkelig plass for tekst-label i plot.
+  # Da kan vi bruke expand-argumentet under.
+  expand_soyle_str_fig = expansion(mult = c(0.0, .09), add = 0)
+
+  # Fjern luft til venstre for y-akseteksten og legg
+  # til ekstra luft til høgre for han, fjern luft under
+  # x-akseteksten og legg til ekstra luft over han,
+  # fjern nesten all luft rundt figurane (eventuell
+  # nødvendig luft legg me til via LaTeX).
+  #
+  # (Merk: Me set den ytre margen til 3 punkt
+  # opp og nede i staden for 0 punkt for å sikra at at
+  # kantane på alle bokstavane alltid vert med.)
+  tema$axis.title.y$margin = margin(r = tema$text$size / 2)
+  tema$axis.title.x$margin = margin(t = tema$text$size / 2)
+  tema$plot.margin = margin(3, 3, 3, 3)
+
+  tema
+}
 
 
 # Graffunksjoner ----------------------------------------------------------
