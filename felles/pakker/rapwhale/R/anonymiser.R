4bd9b64eb0ad5d60d54d8c2fb96728917c201c44
Endre Kvåle Evjen <endre.kvale.evjen@helse-bergen.no>
2020-11-25 13:55:00 +0000

    Lagt inn eksempler i dokumentasjon for lag_ano_funk() og anonymiser()
    SVN-revisjon: 6126

--- felles/pakker/rapwhale/R/anonymiser.R
+++ felles/pakker/rapwhale/R/anonymiser.R
@@ -9,9 +9,25 @@ NULL
 #' anonymiserte ID-ane på tvers av datasetta.
 #'
 #' @param x Ein vektor med ID-ar
-#' @param startnr Startnummeret for dei anonymiserte id-ane (standardverdi = 101)
+#' @param startnr Startnummeret for dei anonymiserte id-ane (standardverdi = 1001)
 #' @export
-
+#' 
+#' @examples 
+#' Eksempeldata: 
+#' id = seq(1, 100, 1)
+#' syk = sample(x = c("Haukeland", "Tromsø", "Trondheim"), size = 100, replace = TRUE)
+#' diag_kode = sample(1:5, size = 100, replace = TRUE)
+#'
+#' a = tibble::tibble(PasientID = id, Sykehus = syk)
+#' b = tibble::tibble(PasientID = id, diagnosekode = diag_kode)
+#' 
+#' ano_funksjon = lag_ano_funk(x = a$PasientID, startnr = 101)
+#' 
+#' a$lpnr = ano_funksjon(a$PasientID)
+#' b$lpnr = ano_funksjon(b$PasientID)
+#' 
+#' Løpenummer blir her samme tall for pasientene på tvers av datasett. 
+#' 
 lag_ano_funk = function(x, startnr = 1001) {
   if (any(is.na(x))) {
     warning("ID-vektoren inneheld NA-verdiar")
@@ -24,7 +40,7 @@ lag_ano_funk = function(x, startnr = 1001) {
       warning("ID-vektoren inneheld NA-verdiar")
     }
     if (!is_empty(na.omit(setdiff(x_utvalg, x)))) {
-      warning("ID-vektoren inneheld nye (ukjende) ID-ar")
+      warning ("ID-vektoren inneheld nye (ukjende) ID-ar")
     }
     til[match(x_utvalg, fra)]
   }
@@ -32,15 +48,24 @@ lag_ano_funk = function(x, startnr = 1001) {
 }
 
 #' Anonymiseringsfunksjon
+#' 
+#' Hjelpefunksjon for lag_ano_funk() som kan brukes hvis det bare er ett datasett som skal anonymiseres. 
+#' Funksjonen gjør at du slipper å kalle anonymiseringsfunksjonen på det samme datasettet to ganger. 
+#' Tar inn en vektor med ID'er og returnerer en vektor med ny numerisk id. 
 #'
-#' Hjelpefunksjon for lag_ano_funk() som kan brukes hvis det bare er ett datasett som skal anonymiseres.
-#' Funksjonen gjør at du slipper å kalle anonymiseringsfunksjonen på det samme datasettet to ganger.
-#' Tar inn en vektor med ID'er og returnerer en vektor med ny numerisk id.
-#'
-#' @param x vektor med ID'er som skal anonymiseres.
-#' @param startnr løpenummer for ny ID. Starnr definerer første nummer i rekken.
+#' @param x vektor med ID'er som skal anonymiseres. 
+#' @param startnr løpenummer for ny ID. Startnr definerer første nummer i rekken.
 #'
 #' @export
+#' 
+#' @examples 
+#' Eksempeldata:
+#' a = tibble::tibble(PasientID = seq(1, 100, 1), 
+#' Sykehus = sample(x = c("Haukeland", "Tromsø", "Trondheim"), 
+#' size = 100, replace = TRUE))
+#' 
+#' anonymisert_id = anonymiser(a$PasientID, startnr = 1001)
+#' 
 anonymiser = function(x, startnr = 1001) {
   lag_ano_funk(x, startnr = startnr)(x)
 }
