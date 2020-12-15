6922824e8785be9c1fb688a5e7a055cd3648fa5a
Karl Ove Hufthammer <karl.ove.hufthammer@helse-bergen.no>
2020-12-15 14:18:28 +0000

    Rydda/forbetra litt testar for kjed_ord()
    SVN-revisjon: 6179

--- felles/pakker/rapwhale/tests/testthat/test-kjed_ord.R
+++ felles/pakker/rapwhale/tests/testthat/test-kjed_ord.R
@@ -1,29 +1,26 @@
-# Eksempeldata
-namn1 = c("Per")
-namn2 = c("Per", "Kari")
-namn3 = c("Per", "Kari", "Ola")
-
-test_that("Gjev ut rett mengd element, og av rett type", {
-  expect_length(kjed_ord(namn3), 1)
-  expect_type(kjed_ord(namn3), "character")
+test_that("Gjev ut rett resultat for vektor med 1, 2, 3 eller 4/fleire element", {
+  expect_identical(kjed_ord("Per"), "Per")
+  expect_identical(kjed_ord(c("Per", "Kari")), "Per og Kari")
+  expect_identical(kjed_ord(c("Per", "Kari", "Ola")), "Per, Kari og Ola")
+  expect_identical(kjed_ord(c("Per", "Kari", "Ola", "Sakhwa")), "Per, Kari, Ola og Sakhwa")
 })
 
-test_that("Gjev ut rett resultat for vektor med 1, 2 og 3 element", {
-  expect_identical(kjed_ord(namn1), "Per")
-  expect_identical(kjed_ord(namn2), "Per og Kari")
-  expect_identical(kjed_ord(namn3), "Per, Kari og Ola")
+test_that("Gjev ut rett resultat med valt «skiljeteikn»-argument", {
+  expect_identical(kjed_ord(c("Per", "Kari", "Ola", "Sakhwa"), skiljeteikn = "/"),
+                   "Per/Kari/Ola og Sakhwa")
+}
+
+test_that("Gjev ut rett resultat med valt «og»-argument", {
+  expect_identical(kjed_ord(c("Per", "Kari"), og = "/"), "Per/Kari")
+  expect_identical(kjed_ord(c("Per", "Kari"), og = " & "), "Per & Kari")
+  expect_identical(kjed_ord(c("Per", "Kari", "Ola", "Sakhwa"), og = " & "),
+                   "Per, Kari, Ola & Sakhwa")
 })
 
-test_that("Gjev ut rett resultat med vald `skiljeteikn` og `og`", {
-  expect_identical(kjed_ord(namn3, skiljeteikn = "/"), "Per/Kari og Ola")
-  expect_identical(kjed_ord(namn2, og = " & "), "Per & Kari")
-  expect_identical(kjed_ord(namn3, og = " & "), "Per, Kari & Ola")
+test_that("Ved tom vektor inn får ein tom tekstvektor ut", {
+  expect_identical(kjed_ord(character()), character())
 })
 
-test_that("Ved tom vektor inn skal tom tekstvektor gjevast ut", {
-  expect_identical(kjed_ord(NULL), character(0))
-})
-
-test_that("Skal endra NA-verdiar til tekst", {
+test_that("Gjev ut NA-verdiar som teksten «NA»", {
   expect_identical(kjed_ord(NA), "NA")
 })
