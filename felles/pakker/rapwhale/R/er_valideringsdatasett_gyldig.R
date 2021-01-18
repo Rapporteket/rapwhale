a290465e872524ca12afab8d17c01dae39c7e54f
Per Erik Haugedal <per.erik.haugedal@helse-bergen.no>
2021-01-18 16:25:39 +0000

    Sjekk at inndata er datasett og med rette kolonner
    SVN-revisjon: 6217

--- felles/pakker/rapwhale/R/er_valideringsdatasett_gyldig.R
+++ felles/pakker/rapwhale/R/er_valideringsdatasett_gyldig.R
@@ -1,5 +1,11 @@
 er_valideringsdatasett_gyldig = function(d_vld) {
   gyldig = TRUE
 
+  if (!(is.data.frame(d_vld) &&
+    all(has_name(d_vld, c("vld_varnamn", "vld_vartype")))
+  )) {
+    gyldig = FALSE
+  }
+
   gyldig
 }
