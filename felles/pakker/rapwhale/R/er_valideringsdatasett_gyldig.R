23993b74ff0cb1415bc96747c4c494aeb39de83d
Per Erik Haugedal <per.erik.haugedal@helse-bergen.no>
2021-01-19 15:15:10 +0000

    Sjekk at vld_varnamn og vld_vartype er tekstkolonnAr :)
    SVN-revisjon: 6227

--- felles/pakker/rapwhale/R/er_valideringsdatasett_gyldig.R
+++ felles/pakker/rapwhale/R/er_valideringsdatasett_gyldig.R
@@ -5,6 +5,9 @@ er_valideringsdatasett_gyldig = function(d_vld) {
     all(has_name(d_vld, c("vld_varnamn", "vld_vartype")))
   )) {
     gyldig = FALSE
+  } else if (class(d_vld$vld_varnamn) != "character" ||
+    class(d_vld$vld_vartype) != "character") {
+    gyldig = FALSE
   } else if (anyNA(d_vld$vld_varnamn) ||
     anyNA(d_vld$vld_vartype) ||
     any(d_vld$vld_varnamn == "") ||
