cb0873c3b8ece83f33cbd7b6b40dcf718d612377
Per Erik Haugedal <per.erik.haugedal@helse-bergen.no>
2021-02-02 15:02:48 +0000

    Fiksa samanlikn_identisk() slik at den handterar NA-verdiar
    SVN-revisjon: 6299

--- felles/pakker/rapwhale/R/samanliknarar.R
+++ felles/pakker/rapwhale/R/samanliknarar.R
@@ -1,3 +1,6 @@
 samanlikn_identisk = function(varnamn, verdi1, verdi2) {
-  verdi1 == verdi2
+  # Utdata skal seia (elementvis) om verdi1 er lik verdi2 og skal handtera NA-verdiar
+  like = (verdi1 == verdi2) | (is.na(verdi1) & is.na(verdi2))
+  like[is.na(like)] = FALSE
+  return(like)
 }
