0bd0eed93e306177f4f209321871d5bd641b51b8
Per Erik Haugedal <per.erik.haugedal@helse-bergen.no>
2021-02-03 08:46:15 +0000

    Sjekk om inndata er på rett format
    SVN-revisjon: 6302

--- felles/pakker/rapwhale/R/analyser_valideringsdatasett.R
+++ felles/pakker/rapwhale/R/analyser_valideringsdatasett.R
@@ -1,3 +1,6 @@
 analyser_valideringsdatasett = function(d_vld, samanliknar = samanlikn_identisk) {
-
+  # Skal stoppa med feilmelding dersom inndata ikkje er gyldig
+  if (!er_valideringsdatasett_gyldig(d_vld)) {
+    stop("Datasettet er ikkje på rett format")
+  }
 }
