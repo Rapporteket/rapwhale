5986ddbf318c1918d4c31cf3db3355c4f67b2052
Per Erik Haugedal <per.erik.haugedal@helse-bergen.no>
2021-02-04 14:16:55 +0000

    Lagt til dokumentasjon på analyser_valideringsdatasett()
    SVN-revisjon: 6316

--- felles/pakker/rapwhale/R/analyser_valideringsdatasett.R
+++ felles/pakker/rapwhale/R/analyser_valideringsdatasett.R
@@ -1,3 +1,50 @@
+#' Analyser valideringsdatasett
+#'
+#' @description Tek inn eit datasett og ein samanliknarfunksjon, og returnerer
+#'              datasettet med ein ekstra kolonne med TRUE/FALSE for kvar rad
+#'              alt etter om verdiane som samanliknast er «like».
+#'
+#' @param d_vld Datasett som skal validerast.
+#' @param samanliknar Samanliknarfunksjon, standard `samanlikn_identisk()`.
+#'
+#' @details
+#' Funksjonen tek inn ein datasett `d_vld` og ein samanliknarfunksjon
+#' `samanliknar`. `d_vld` må vera på rett format, dvs.
+#' `er_valideringsdatasett_gyldig(d_vld)` må vera TRUE.
+#'
+#' `samanliknar` skal vera ein funksjon som tek inn tre like lange vektorar,
+#' der den fyrste inneheld namna på variablane, og dei to siste er verdiane som
+#' skal samanliknast elementvis. Den skal returnera ein logisk vektor med same
+#' lengd som vektorane den tek inn, som er TRUE/FALSE alt etter om elementa er
+#' «like». Kva som vert rekna som «like» varierar mellom ulike samanliknarfunksjonar.
+#'
+#' `analyser_valideringsdatasett()` brukar `samanliknar` til å samanlikna
+#' `vld_verdi_intern_x` og `vld_verdi_ekstern_x` for kvar rad der `vld_vartype = x`.
+#'
+#' Til slutt returnerast det opphavlege datasettet `d_vld` med ein ekstra
+#' kolonne `vld_verdi_er_like` som er TRUE/FALSE for kvar rad alt etter om
+#' verdiane som vart samanlikna er «like».
+#'
+#' @return Returnerer opphavleg datasett med ein ekstra kolonne `vld_verdi_er_like`
+#'         som er TRUE/FALSE for kvar rad alt etter om verdiane som skal
+#'         samanliknast på den rada er «like» i følge `samanliknar`.
+#' @export
+#'
+#' @examples
+#' d_vld = tibble(
+#'   pasid = 101, vld_varnamn = "vekt", vld_vartype = "tal",
+#'   vld_verdi_intern_tal = 76,
+#'   vld_verdi_ekstern_tal = as.numeric(NA)
+#' )
+#' analyser_valideringsdatasett(d_vld)
+#'
+#' d_vld = tibble(
+#'   pasid = c(101:104), vld_varnamn = rep("diabetes", 4),
+#'   vld_vartype = rep("logisk", 4),
+#'   vld_verdi_intern_logisk = c(TRUE, NA, FALSE, TRUE),
+#'   vld_verdi_ekstern_logisk = c(TRUE, NA, TRUE, NA)
+#' )
+#' analyser_valideringsdatasett(d_vld)
 analyser_valideringsdatasett = function(d_vld, samanliknar = samanlikn_identisk) {
   # Skal stoppa med feilmelding dersom inndata ikkje er gyldig
   if (!er_valideringsdatasett_gyldig(d_vld)) {
