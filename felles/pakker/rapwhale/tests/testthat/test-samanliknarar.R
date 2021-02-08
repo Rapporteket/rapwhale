04742feb879b1b2b4af2f7b3a2662f30e96e83e3
Karl Ove Hufthammer <karl.ove.hufthammer@helse-bergen.no>
2021-02-08 13:18:41 +0000

    Meir komplette testar for samanlikn_identisk()
    SVN-revisjon: 6342

--- felles/pakker/rapwhale/tests/testthat/test-samanliknarar.R
+++ felles/pakker/rapwhale/tests/testthat/test-samanliknarar.R
@@ -1,29 +1,38 @@
 test_that("Utdata skal seia (elementvis) om verdi1 er lik verdi2", {
   expect_identical(
     samanlikn_identisk(
-      varnamn = "dato_ut",
-      verdi1 = as.Date("2020-06-07"),
-      verdi2 = as.Date("2020-06-07")
+      varnamn = rep("vekt", 5),
+      verdi1 = c(74, 74, 74, NA, NA),
+      verdi2 = c(74, 80, NA, 74, NA)
     ),
-    TRUE
+    c(TRUE, FALSE, FALSE, FALSE, TRUE)
   )
 
   expect_identical(
     samanlikn_identisk(
-      varnamn = rep("vekt", 4),
-      verdi1 = c(74, 72, 50, 63),
-      verdi2 = c(74, 72, 50, 63)
+      varnamn = rep("dato", 5),
+      verdi1 = c(74, 74, 74, NA, NA) + Sys.Date(),
+      verdi2 = c(74, 80, NA, 74, NA) + Sys.Date()
     ),
-    rep(TRUE, 4)
+    c(TRUE, FALSE, FALSE, FALSE, TRUE)
   )
 
   expect_identical(
     samanlikn_identisk(
-      varnamn = rep("diabetes", 4),
-      verdi1 = c(TRUE, TRUE, FALSE, TRUE),
-      verdi2 = c(TRUE, TRUE, FALSE, FALSE)
+      varnamn = rep("vekt", 7),
+      verdi1 = c("foo", "foo", "", "", "NA", NA, NA),
+      verdi2 = c("foo", "bar", NA, "", NA, "", NA)
     ),
-    c(TRUE, TRUE, TRUE, FALSE)
+    c(TRUE, FALSE, FALSE, TRUE, FALSE, FALSE, TRUE)
+  )
+
+  expect_identical(
+    samanlikn_identisk(
+      varnamn = rep("janei", 8),
+      verdi1 = c(TRUE, TRUE, FALSE, TRUE, FALSE, NA, NA, NA),
+      verdi2 = c(TRUE, FALSE, TRUE, NA, NA, TRUE, FALSE, NA)
+    ),
+    c(TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE)
   )
 })
 
