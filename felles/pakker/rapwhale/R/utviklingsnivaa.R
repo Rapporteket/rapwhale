fef75662c656205a1b234732418ea14ff64da095
Per Erik Haugedal <per.erik.haugedal@helse-bergen.no>
2021-03-09 09:19:30 +0000

    Lagt til dokumentasjon for utviklingsnivaa()
    SVN-revisjon: 6536

--- felles/pakker/rapwhale/R/utviklingsnivaa.R
+++ felles/pakker/rapwhale/R/utviklingsnivaa.R
@@ -1,3 +1,27 @@
+#' Lag oversikt over utviklingsnivå
+#'
+#' @description
+#' `r lifecycle::badge("experimental")`
+#'
+#' Sjekkar alle .Rd-filer i ei mappe, og lagar ein oversikt over utviklingsnivåa.
+#'
+#' @param mappe Adresse til mappe med hjelpefiler. Standard er "H:/kvalreg/felles/pakker/rapwhale/man".
+#'
+#' @details
+#' Funksjonen tek inn adressa til ei mappe, og ser etter utviklingsnivå
+#' (`lifecycle`-badge) i alle .Rd-filene i mappa. Den gjev så ut ein oversikt
+#' over alle funksjonsnamn og utviklingsnivå i en tibble.
+#'
+#'
+#' @return Tibble med kolonnane `funksjon` og `utviklingsnivaa` som gjev ein
+#'         oversikt over utviklingsnivåa til alle funksjonane som har
+#'         hjelpefil i `mappe`.
+#' @export
+#'
+#' @examples
+#' utviklingsnivaa()
+#'
+#' utviklingsnivaa("H:/kvalreg/felles/pakker/rapwhale/man")
 utviklingsnivaa = function(mappe = "H:/kvalreg/felles/pakker/rapwhale/man") {
   # Lag vektor med adresser til hjelpefilene
   funksjonar = list.files(mappe, pattern = "\\.Rd$", full.names = TRUE)
