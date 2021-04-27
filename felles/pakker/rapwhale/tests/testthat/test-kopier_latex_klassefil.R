b410dfa6a530bc35a10a0bf7253ce31ea4fc3323
Per Erik Haugedal <per.erik.haugedal@helse-bergen.no>
2021-04-27 09:31:17 +0000

    Manglande skråstrek i filadresse
    SVN-revisjon: 6811

--- felles/pakker/rapwhale/tests/testthat/test-kopier_latex_klassefil.R
+++ felles/pakker/rapwhale/tests/testthat/test-kopier_latex_klassefil.R
@@ -1,5 +1,5 @@
 test_that("LaTeX-klassefila vert kopiert til rett mappe", {
   tempmappe = tempdir()
   kopier_latex_klassefil(tempmappe)
-  expect_true(file.exists(paste0(tempmappe, "kvalreg-rapport.cls")))
+  expect_true(file.exists(paste0(tempmappe, "/kvalreg-rapport.cls")))
 })
