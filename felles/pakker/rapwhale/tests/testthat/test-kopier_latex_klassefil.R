60f0849e2548e625638c65e7458b54971bb3c617
Per Erik Haugedal <per.erik.haugedal@helse-bergen.no>
2021-05-05 11:23:11 +0000

    Test på at kopier_latex_klassefil() gjev ut adressa det kopierast til usynleg
    SVN-revisjon: 6853

--- felles/pakker/rapwhale/tests/testthat/test-kopier_latex_klassefil.R
+++ felles/pakker/rapwhale/tests/testthat/test-kopier_latex_klassefil.R
@@ -1,6 +1,10 @@
 tempmappe = tempdir()
-kopier_latex_klassefil(tempmappe)
 klassefil_adresse = paste0(tempmappe, "/kvalreg-rapport.cls")
+
+test_that("Gjev ut adressa til mappa det kopierast til (usynleg)", {
+  adresse = expect_invisible(kopier_latex_klassefil(tempmappe))
+  expect_identical(adresse, tempmappe)
+})
 
 test_that("LaTeX-klassefila vert kopiert til rett mappe", {
   expect_true(file.exists(klassefil_adresse))
