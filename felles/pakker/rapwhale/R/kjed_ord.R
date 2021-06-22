875e40d20baf3f58b16ff87415191b9c57e4c19a
Karl Ove Hufthammer <karl.ove.hufthammer@helse-bergen.no>
2021-06-22 13:21:06 +0000

    Forsøk på forbetring av hjelpeteksten til kjed_ord()
    SVN-revisjon: 7087

--- felles/pakker/rapwhale/R/kjed_ord.R
+++ felles/pakker/rapwhale/R/kjed_ord.R
@@ -1,24 +1,36 @@
-#' Kjede saman element
+#' Kjed saman elementa i ein vektor til ein tekststreng
 #'
 #' @description
 #' `r lifecycle::badge("maturing")`
 #'
-#' Tek inn ein vektor, kjedar saman elementa og returnerar
-#' dei som ein tekststreng.
+#' Tek inn ein vektor og kjedar saman (listar opp) elementa.
+#' Returnerer resultatet som ein tekststreng.
 #'
 #' @param ord Ein vektor med dei elementa som skal kjedast saman.
-#' @param skiljeteikn Skiljeteikn mellom elementa. Standard er ", ".
-#' @param og Tekst som vert sett inn mellom dei to siste elementa. Standard er " og ".
-#' @export
+#' @param skiljeteikn Skiljeteikn som skal brukast mellom alle
+#'   elementa (utanom dei to siste).
+#' @param og Tekst som skal setjast inn mellom dei to siste elementa.
 #'
 #' @details
-#' Funksjonen tek inn ein vektor, og kjedar saman elementa med `skiljeteikn`
-#' mellom kvart av dei, utanom dei to siste, som vert skilde med `og`.
-#' Resultatet vert returnert som ein tekststreng med eitt element. Funksjonen
-#' handterar ulike typar inndata, som tekst, tal og dato. Manglande verdiar
-#' vert gjort om til teksten "NA".
+#' Funksjonen er meint å brukast til å laga ei opplisting av
+#' elementa i `ord`
+#' (typisk ein tekst- eller talvektor)
+#' på ein naturleg måte.
 #'
-#' @return Ein tekststreng med eitt element, der alle elementa frå `ord` er lista opp
+#' Funksjonen tek inn ein vektor og kjedar saman elementa med `skiljeteikn`
+#' (som standard eit komma)
+#' mellom kvart element, utanom dei to siste, som vert kjeda saman med `og`
+#' (som standard ordet «og» med mellomrom rundt).
+#' Resultatet vert returnert som ein tekststreng med eitt element.
+#'
+#' Funksjonen handterar ulike typar inndata, som tekst, tal, dato
+#' og andre typar objekt der [base::as.character()] er definert.
+#' Manglande verdiar vert gjort om til teksten `"NA"`.
+#'
+#' @return Ein tekststreng med eitt element,
+#'   der alle elementa frå `ord` er kjeda saman.
+#'
+#' @export
 #'
 #' @examples
 #' kjed_ord(c("Per", "Kari"))
