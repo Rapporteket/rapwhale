301e7eb9cc28998472a2b30c4d634e0449b9fd8a
Karl Ove Hufthammer <karl.ove.hufthammer@helse-bergen.no>
2021-06-22 10:39:06 +0000

    Meir komplett dokumentasjon og eksempel for anonymiseringsfunksjonar
    Det var ganske uklart kva dei ulike funksjonane faktisk var meint å gjera,
    kva innverdiar dei tok og kva utverdiar dei gav ut. Dokumentasjonen
    og eksempel er derfor meir eller mindre skrivne på nytt frå grunnen av.
    
    SVN-revisjon: 7071

--- felles/pakker/rapwhale/R/anonymiser.R
+++ felles/pakker/rapwhale/R/anonymiser.R
@@ -1,36 +1,69 @@
 #' @importFrom rlang is_empty
 NULL
 
-#' Anonymiseringsfunksjonfunksjon
-#' 
-#' @description 
+#' Lag funksjon for anonymisering av datavektorar
+#'
+#' @description
 #' `r lifecycle::badge("maturing")`
 #'
-#' Funksjonen lag_ano_funk() tar inn to argument og gjev ut ein
-#' anonymiseringsfunksjon (anonymiser_x_utvalg) som fungerer
-#' slik at ID-ar (som finst i fleire datasett) får dei same
-#' anonymiserte ID-ane på tvers av datasetta.
+#' Lagar ein funksjon som kan brukast til å anonymisera verdiane i ein vektor
+#' ved å byta dei ut med tilfeldige tal.
+#' Funksjonen ein får ut,
+#' er deterministisk, dvs. han gjev alltid ut same utverdi
+#' («tilfeldige» tal)
+#' for same innverdi.
 #'
-#' @param x Ein vektor med ID-ar
-#' @param startnr Startnummeret for dei anonymiserte id-ane (standardverdi = 1001)
+#' @param x Vektor (av valfri type/klasse) med verdiar som skal anonymiserast.
+#' @param startnr Minste verdi for dei tilfeldige tala som skal genererast.
+#'
+#' @details
+#' Denne funksjonen er meint å brukast når ein har verdiar
+#' (for eksempel pasient-ID-ar)
+#' som inngår i fleire datasett og som skal anonymiserast.
+#' Då er det viktig at dei vert anonymiserte til same verdiar
+#' på tvers av datasetta.
+#'
+#' Sjå [anonymiser()] for informasjon om argumenta og detaljar
+#' om anonymiseringa (desse er like i denne funksjonen).
+#'
+#' @note
+#' Funksjonen må køyrast på ein vektor som inneheld *alle*
+#' verdiane ein i framtida ønskjer å anonymisera.
+#' (Viss ein vil anonymisera pasient-ID-ar i eit kvalitetsregister,
+#' er dette typisk data frå eit inklusjonsskjema eller eit pasientskjema).
+#' Viss det ikkje finst ein slik vektor frå før,
+#' må ein laga ein,
+#' ved å samla alle verdiane ein vil anonymisera på tvers av datasett.
+#'
+#' @seealso
+#' Bruk heller [anonymiser()] dersom du berre vil anonymisera éin vektor.
+#'
+#' @return
+#' Funksjon som tar inn ein vektor med verdiar
+#' (som alle må inngå i `x`)
+#' og gjev ut ein heiltalsvektor med tilsvarande anonymiserte verdiar.
+#'
 #' @export
-#' 
-#' @examples 
-#' Eksempeldata: 
-#' id = seq(1, 100, 1)
-#' syk = sample(x = c("Haukeland", "Tromsø", "Trondheim"), size = 100, replace = TRUE)
-#' diag_kode = sample(1:5, size = 100, replace = TRUE)
 #'
-#' a = tibble::tibble(PasientID = id, Sykehus = syk)
-#' b = tibble::tibble(PasientID = id, diagnosekode = diag_kode)
-#' 
-#' ano_funksjon = lag_ano_funk(x = a$PasientID, startnr = 101)
-#' 
-#' a$lpnr = ano_funksjon(a$PasientID)
-#' b$lpnr = ano_funksjon(b$PasientID)
-#' 
-#' Løpenummer blir her samme tall for pasientene på tvers av datasett. 
-#' 
+#' @examples
+#' d_operasjon = tibble::tibble(
+#'   pasient_id = c(2, 5, 7, 5, 8),
+#'   sjukehus = c(
+#'     "Bergen", "Oslo", "Stavanger",
+#'     "Oslo", "Stavanger"
+#'   )
+#' )
+#' d_oppfolging = tibble::tibble(
+#'   pasient_id = c(7, 8),
+#'   komplikasjonar = c(TRUE, FALSE)
+#' )
+#'
+#' anonymiser_pasids = lag_ano_funk(d_operajson$pasient_id)
+#'
+#' d_operasjon_ano = dplyr::mutate(d_operasjon, pasient_id = anonymiser_pasids(pasient_id))
+#' d_oppfolging_ano = dplyr::mutate(d_oppfolging, pasient_id = anonymiser_pasids(pasient_id))
+#' d_operasjon_ano
+#' d_oppfolging_ano
 lag_ano_funk = function(x, startnr = 1001) {
   if (any(is.na(x))) {
     warning("ID-vektoren inneheld NA-verdiar")
@@ -43,35 +76,56 @@ lag_ano_funk = function(x, startnr = 1001) {
       warning("ID-vektoren inneheld NA-verdiar")
     }
     if (!is_empty(na.omit(setdiff(x_utvalg, x)))) {
-      warning ("ID-vektoren inneheld nye (ukjende) ID-ar")
+      warning("ID-vektoren inneheld nye (ukjende) ID-ar")
     }
     til[match(x_utvalg, fra)]
   }
   ano_funk
 }
 
-#' Anonymiseringsfunksjon
-#' 
-#' @description 
-#' `r lifecycle::badge("maturing")`
-#' 
-#' Hjelpefunksjon for lag_ano_funk() som kan brukes hvis det bare er ett datasett som skal anonymiseres. 
-#' Funksjonen gjør at du slipper å kalle anonymiseringsfunksjonen på det samme datasettet to ganger. 
-#' Tar inn en vektor med ID'er og returnerer en vektor med ny numerisk id. 
+#' Anonymiser datavektor
 #'
-#' @param x vektor med ID'er som skal anonymiseres. 
-#' @param startnr løpenummer for ny ID. Startnr definerer første nummer i rekken.
+#' @description
+#' `r lifecycle::badge("maturing")`
+#'
+#' Anonymiserer verdiane i ein vektor ved å byta dei ut med tilfeldige tal.
+#'
+#' @inheritParams lag_ano_funk
+#'
+#' @details
+#' Alle verdiane i  `x` vert bytte ut med tilfeldige tal frå følgja
+#' `startnr`, `startnr + 1`, `startnr + 2`, …,
+#' men slik at viss ein verdi finst fleire gongar i `x`,
+#' får han alltid same (tilfeldige) utverdi.
+#'
+#' Innverdiane kan vera av valfri type, for eksempel tekst,
+#' tal eller datoar,
+#' men utverdiane vil alltid vera heiltal.
+#' Eventuelle `NA`-verdiar kjem ut som `NA`-verdiar,
+#' men med ei åtvaring.
+#'
+#' @seealso
+#' Viss du vil anonymisera verdiar som inngår i fleire datasett,
+#' bruk heller [lag_ano_funk()].
+#'
+#' @return
+#' Heiltalsvektor med anonymiserte verdiar.
 #'
 #' @export
-#' 
-#' @examples 
-#' Eksempeldata:
-#' a = tibble::tibble(PasientID = seq(1, 100, 1), 
-#' Sykehus = sample(x = c("Haukeland", "Tromsø", "Trondheim"), 
-#' size = 100, replace = TRUE))
-#' 
-#' anonymisert_id = anonymiser(a$PasientID, startnr = 1001)
-#' 
+#'
+#' @examples
+#' d = tibble::tibble(
+#'   pasient_id = c(2, 5, 7, 5, 8),
+#'   sjukehus = c(
+#'     "Bergen", "Oslo", "Stavanger",
+#'     "Oslo", "Stavanger"
+#'   )
+#' )
+#'
+#' dplyr::mutate(d,
+#'   pasient_id_ano = anonymiser(pasient_id),
+#'   sjukehus_ano = anonymiser(sjukehus, startnr = 11)
+#' )
 anonymiser = function(x, startnr = 1001) {
   lag_ano_funk(x, startnr = startnr)(x)
 }
