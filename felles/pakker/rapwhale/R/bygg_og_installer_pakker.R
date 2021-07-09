9c4ffe9260ad594ef89485cbad26eb3be5f64d24
Per Erik Haugedal <per.erik.haugedal@helse-bergen.no>
2021-07-09 07:57:51 +0000

    Mellombels slått av bygging av vignettar i bygg_og_installer_pakker()
    Får p.t. feilmeldinga «pandoc: openBinaryFile: does not exist» ved bygging av vignettar. Slår derfor mellombels av bygging av dei.
    
    SVN-revisjon: 7134

--- felles/pakker/rapwhale/R/bygg_og_installer_pakker.R
+++ felles/pakker/rapwhale/R/bygg_og_installer_pakker.R
@@ -25,7 +25,7 @@ bygg_og_installer_pakker = function(pakkemappe = "H:\\kvalreg\\felles\\pakker\\"
     adresse_pakke = paste0(pakkemappe, pakke)
     cat("Dokumenter og bygg tar.gz-fil:", pakke, "\n")
     devtools::document(adresse_pakke,
-      roclets = c("rd", "collate", "namespace", "vignette"),
+      roclets = c("rd", "collate", "namespace"),
       quiet = stille
     )
     adresse_pakkefil = devtools::build(adresse_pakke, quiet = stille)
