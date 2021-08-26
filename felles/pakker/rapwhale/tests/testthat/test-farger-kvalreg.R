f6c21a5844eec6f1d1fddaad97fa2d43e0ba9aee
Endre Kvåle Evjen <endre.kvale.evjen@helse-bergen.no>
2021-08-26 11:05:08 +0000

    Lagt inn (enkle) tester for farge_morkare
    SVN-revisjon: 7206

---
+++ felles/pakker/rapwhale/tests/testthat/test-farger-kvalreg.R
@@ -0,0 +1,10 @@
+context("farge_morkare")
+test_that("farge_morkare gir ut forventede fargekoder ved ulike grader", {
+  farger = c("#000059", "#084594", "#2171b5")
+
+  forventet_grad_5 = c("#00004D", "#003A87", "#0065A7")
+  forventet_grad_minus_5 = c("#170965", "#2450A1", "#367DC3")
+
+  expect_identical(farge_morkare(farger, grad = 5), forventet_grad_5)
+  expect_identical(farge_morkare(farger, grad = -5), forventet_grad_minus_5)
+})
