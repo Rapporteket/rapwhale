767b1153e9acd146d6a244354e765e73e2f8d9c1
Karl Ove Hufthammer <karl.ove.hufthammer@helse-bergen.no>
2021-08-26 12:29:42 +0000

    Lagt til testar for er_gyldig_fnr_dato()
    SVN-revisjon: 7220

--- felles/pakker/rapwhale/tests/testthat/test-fodselsnummer-funksjoner.R
+++ felles/pakker/rapwhale/tests/testthat/test-fodselsnummer-funksjoner.R
@@ -8,3 +8,19 @@ test_that("er_syntaktisk_fnr() gjev forventa resultat", {
   forventa = c(FALSE, FALSE, TRUE, FALSE, FALSE)
   expect_identical(er_syntaktisk_fnr(nummer), forventa)
 })
+
+
+
+context("er_gyldig_fnr_dato()")
+
+test_that("er_gyldig_fnr_dato() gjev forventa resultat", {
+  datoar = c(
+    "010101", "170527", "311299",
+    "290200", # Skotårsdag, gyldig i år 2000, men ikkje i 1900 ...
+    "320101", "011382", "999999",
+    "290225", # «Skotårsdag» som aldri er gyldig ...
+    "241224"
+  )
+  dato_gyldig = c(TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, TRUE)
+  expect_identical(er_gyldig_fnr_dato(datoar), dato_gyldig)
+})
