c74f5e8c7b5890df8ea271876e42eca1ce5f0549
Karl Ove Hufthammer <karl.ove.hufthammer@helse-bergen.no>
2021-08-26 13:00:56 +0000

    Lagt til testar for er_gyldig_f_nummer()
    SVN-revisjon: 7226

--- felles/pakker/rapwhale/tests/testthat/test-fodselsnummer-funksjoner.R
+++ felles/pakker/rapwhale/tests/testthat/test-fodselsnummer-funksjoner.R
@@ -24,3 +24,41 @@ test_that("er_gyldig_fnr_dato() gjev forventa resultat", {
   dato_gyldig = c(TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, TRUE)
   expect_identical(er_gyldig_fnr_dato(datoar), dato_gyldig)
 })
+
+
+
+context("er_gyldig_f_nummer()")
+
+# Dei gyldige fødselsnummera er ikkje reelle,
+# men henta frå dokumentet «Testaktører»
+# versjon 3.7, datert 19.11.2013.
+
+test_that("er_gyldig_f_nummer() gjev forventa resultat", {
+  fnr_gyldige = c(
+    "15076500565", "21016400952",
+    "12057900499", "13116900216",
+    "14019800513", "05073500186"
+  )
+  fnr_ugyldige = c(
+    "15076500561", "21016400992",
+    "13057900499", "13126900216",
+    "98019800547"
+  ) # Siste nummer har gyldige sjekksiffer
+  expect_identical(
+    er_gyldig_f_nummer(fnr_gyldige),
+    rep(TRUE, length(fnr_gyldige))
+  )
+  expect_identical(
+    er_gyldig_f_nummer(fnr_ugyldige),
+    rep(TRUE, length(fnr_ugyldige))
+  )
+})
+
+test_that("er_gyldig_f_nummer() fungerer òg med vektorar av lengd 1", {
+  expect_identical(er_gyldig_f_nummer("15076500565"), TRUE)
+  expect_identical(er_gyldig_f_nummer("15076500561"), FALSE)
+})
+
+test_that("er_gyldig_f_nummer() fungerer òg med vektorar av lengd 0", {
+  expect_identical(er_gyldig_f_nummer(character()), logical())
+})
