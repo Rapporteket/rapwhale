935749533247a17bf2bfeb8835c65d7bf2f591cd
Stian Tufte Veisene <stian.tufte.veisene@helse-bergen.no>
2021-08-26 12:48:09 +0000

    Lagt 3 beslektede tester for akse_prosent_format() inn i samme testthat()
    SVN-revisjon: 7225

--- felles/pakker/rapwhale/tests/testthat/test-rapporttekst-funksjoner.R
+++ felles/pakker/rapwhale/tests/testthat/test-rapporttekst-funksjoner.R
@@ -1,19 +1,17 @@
 # Testing av akse_prosent_format()
 
-test_that("akse_prosent_format() gir ut verdier på riktig format ved 0 desimaler", {
+test_that("akse_prosent_format() gir ut verdier på riktig format ved
+           0, 1 og 2 desimaler", {
   prosent_format_0_desimaler = akse_prosent_format(antall_desimaler = 0)
   expect_identical(prosent_format_0_desimaler(0.0468), "5 %")
-})
 
-test_that("akse_prosent_format() gir ut verdier på riktig format ved 1 desimal", {
   prosent_format_1_desimal = akse_prosent_format()
   expect_identical(prosent_format_1_desimal(0.0468), "4,7 %")
-})
 
-test_that("akse_prosent_format() gir ut verdier på riktig format ved 2 desimaler", {
   prosent_format_2_desimaler = akse_prosent_format(antall_desimaler = 2)
   expect_identical(prosent_format_2_desimaler(0.0468), "4,68 %")
 })
+
 
 test_that("akse_prosent_format() gir ut verdier på riktig format ved å
            oppgi punktum som desimalskilletegn", {
