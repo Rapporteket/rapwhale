863c61512884b844120f7e6a39a616e5b4066ebc
Stian Tufte Veisene <stian.tufte.veisene@helse-bergen.no>
2021-08-31 12:22:41 +0000

    Lagt til vektorer med flere verdier i tester for akse-format-funksjonene
    Siden det nå blir det testet flere ulike verdier i hver
    test ble den siste test_that()-en unødvendig og derfor
    fjernet.
    
    SVN-revisjon: 7248

--- felles/pakker/rapwhale/tests/testthat/test-rapporttekst-funksjoner.R
+++ felles/pakker/rapwhale/tests/testthat/test-rapporttekst-funksjoner.R
@@ -3,20 +3,20 @@
 test_that("akse_prosent_format() gir ut verdier på riktig format ved
            0, 1 og 2 desimaler", {
   prosent_format_0_desimaler = akse_prosent_format(antall_desimaler = 0)
-  expect_identical(prosent_format_0_desimaler(0.0468), "5 %")
+  expect_identical(prosent_format_0_desimaler(c(0.0036, 0.0468, 0.78)), c("0 %", "5 %", "78 %"))
 
   prosent_format_1_desimal = akse_prosent_format()
-  expect_identical(prosent_format_1_desimal(0.0468), "4,7 %")
+  expect_identical(prosent_format_1_desimal(c(0.0036, 0.0468, 0.78)), c("0,4 %", "4,7 %", "78,0 %"))
 
   prosent_format_2_desimaler = akse_prosent_format(antall_desimaler = 2)
-  expect_identical(prosent_format_2_desimaler(0.0468), "4,68 %")
+  expect_identical(prosent_format_2_desimaler(c(0.0036, 0.0468, 0.78)), c("0,36 %", "4,68 %", "78,00 %"))
 })
 
 
 test_that("akse_prosent_format() gir ut verdier på riktig format ved å
            oppgi punktum som desimalskilletegn", {
   prosent_format_punktum_skilletegn = akse_prosent_format(decimal.mark = ".")
-  expect_identical(prosent_format_punktum_skilletegn(0.0468), "4.7 %")
+  expect_identical(prosent_format_punktum_skilletegn(c(0.0036, 0.0468, 0.78)), c("0.4 %", "4.7 %", "78.0 %"))
 })
 
 # Testing av akse_tall_format()
@@ -24,30 +24,23 @@ test_that("akse_prosent_format() gir ut verdier på riktig format ved å
 test_that("akse_tall_format() gir ut verdier på riktig format ved
            0, 1 og 2 desimaler", {
   tall_format_0_desimaler = akse_tall_format(antall_desimaler = 0)
-  expect_identical(tall_format_0_desimaler(468.999), "469")
+  expect_identical(tall_format_0_desimaler(c(468.999, 7685.87, 65473.3, 256473)), c("469", "7 686", "65 473", "256 473"))
 
   tall_format_1_desimal = akse_tall_format(antall_desimaler = 1)
-  expect_identical(tall_format_1_desimal(468.999), "469,0")
+  expect_identical(tall_format_1_desimal(c(468.999, 7685.87, 65473.3, 256473)), c("469,0", "7 685,9", "65 473,3", "256 473,0"))
 
   tall_format_2_desimaler = akse_tall_format()
-  expect_identical(tall_format_2_desimaler(468.999), "469,00")
+  expect_identical(tall_format_2_desimaler(c(468.999, 7685.87, 65473.3, 256473)), c("469,00", "7 685,87", "65 473,30", "256 473,00"))
 })
 
 test_that("akse_tall_format() gir ut verdier på riktig format ved å
            oppgi punktum som desimalskilletegn", {
   tall_format_punktum_skilletegn = akse_tall_format(decimal.mark = ".")
-  expect_identical(tall_format_punktum_skilletegn(468.999), "469.00")
+  expect_identical(tall_format_punktum_skilletegn(c(468.999, 7685.87, 65473.3, 256473)), c("469.00", "7 685.87", "65 473.30", "256 473.00"))
 })
 
 test_that("akse_tall_format() gir ut verdier på riktig format ved å
            oppgi XX som tusenskille", {
   tall_format_uten_tusenskille = akse_tall_format(big.mark = "XX")
-  expect_identical(tall_format_uten_tusenskille(1000), "1XX000,00")
-})
-
-test_that("akse_tall_format() gir ut tusenskille på riktig sted for tall
-           med ulikt antall siffer", {
-  tall_format_2_desimaler = akse_tall_format()
-  expect_identical(tall_format_2_desimaler(10000), "10 000,00")
-  expect_identical(tall_format_2_desimaler(100000), "100 000,00")
+  expect_identical(tall_format_uten_tusenskille(c(468.999, 7685.87, 65473.3, 256473)), c("469,00", "7XX685,87", "65XX473,30", "256XX473,00"))
 })
