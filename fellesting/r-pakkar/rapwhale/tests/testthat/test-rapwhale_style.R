035de22f1b827a9b793e73967e007c3c6adf776f
Per Erik Haugedal <per.erik.haugedal@helse-bergen.no>
2021-10-26 15:20:23 +0200

    Testar for ting som skal vera ulikt styler::tidyverse_style()
    Legg berre til testar for det som skal vera ulikt Tidyverse-stilen,
    då resten er henta frå denne, og er testa i styler-pakken. Her
    legg me til fleire testar etter kvart dersom fleire ting skal
    vera ulikt Tidyverse-stilen.

---
+++ fellesting/r-pakkar/rapwhale/tests/testthat/test-rapwhale_style.R
@@ -0,0 +1,49 @@
+# Testar berre det som skal vera ulikt Tidyverse-stilen
+# Resten er testa i styler-pakken
+
+# Sjå vignette("customizing_styler") og help(test_collection) for detaljar
+# om testing av syler-funksjonar
+
+style_text_rapwhale = purrr::partial(styler::style_text, style = rapwhale_style)
+
+test_that("<- vert gjort om til =", {
+  expect_warning(
+    styler:::test_collection(
+      "rapwhale-style",
+      "^left_assign",
+      transformer = style_text_rapwhale,
+      dry = "on"
+    ),
+    NA
+  )
+})
+
+test_that("= vert ikkje endra", {
+  expect_warning(
+    styler:::test_collection(
+      "rapwhale-style",
+      "^equals",
+      transformer = style_text_rapwhale,
+      dry = "on"
+    ),
+    NA
+  )
+})
+
+test_that("<- vert ikkje endra viss 'tokens' ikkje er med i 'scope'", {
+  style_text_rapwhale_line_breaks = purrr:::partial(
+    styler::style_text,
+    style = rapwhale_style,
+    scope = "line_breaks"
+  )
+
+  expect_warning(
+    styler:::test_collection(
+      "rapwhale-style",
+      "^unchanged_token",
+      transformer = style_text_rapwhale_line_breaks,
+      dry = "on"
+    ),
+    NA
+  )
+})
