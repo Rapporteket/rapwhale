6424c76e2e36e5c8eb9de0bfe4ad37e6a76f1eeb
Per Erik Haugedal <per.erik.haugedal@helse-bergen.no>
2021-10-26 15:13:54 +0200

    Dokumentasjon og plasshaldarimplementering for kodestil-funksjon
    Funksjonen tek utgangspunkt i Tidyverse-stilen
    styler::tidyverse_style().

---
+++ fellesting/r-pakkar/rapwhale/R/rapwhale_style.R
@@ -0,0 +1,89 @@
+#' @importFrom styler tidyverse_style tidyverse_reindention tidyverse_math_token_spacing
+NULL
+
+#' Rapwhale-kodestil
+#'
+#' @description
+#' `r lifecycle::badge("experimental")`
+#'
+#' Formater kode etter Rapwhale-kodestilen.
+#'
+#' @param scope
+#' Omfanget av manipulasjon. Kan variera frå `"none"` (minst inngripande) til
+#' `"tokens"` (mest inngripande). Sjå 'Details'.
+#' Dette argumentet er ein tekststreng eller ein vektor av klassen `AsIs`.
+#' @param strict
+#' Ein logisk verdi som indikerer om eit sett med strenge eller ikkje så strenge
+#' transformasjonsfunksjonar skal returnerast.
+#' Samanlikn funksjonane returnert med eller utan `strict = TRUE`.
+#' Til dømes vil det med `strict = TRUE` tvingast eitt mellomrom etter ",",
+#' og eitt linjeskift etter avsluttande krøllparentes.
+#' Med `strict = FALSE` vil det leggjast til mellomrom og linjeskift viss det
+#' ikkje finst, men ikkje endrast noko viss det finst fleire. Sjå Examples'.
+#' @param indent_by
+#' Kor mange mellomrom med innrykk skal setjast inn etter operatorar som `(`.
+#' @param start_comments_with_one_space
+#' Skal kommentarar starta med berre eitt mellomrom
+#' (sjå [styler::start_comments_with_space()]).
+#' @param reindention
+#' Ei liste over parametrar for regex re-innrykk, mest føremålstenleg laga med
+#' [styler::specify_reindention()].
+#' @param math_token_spacing
+#' Ei liste over parametrar som definerar mellomrom i kring matematiske teikn,
+#' føremålstenleg laga med [styler::specify_math_token_spacing()].
+#'
+#' @details
+#' Rapwhale-kodestilen er lik Tidyverse-stilen [styler::tidyverse_style()],
+#' med ei lita endring:
+#' Den brukar `=`-operatoren i staden for `<-`-operatoren.
+#'
+#' Fylgjande nivå for `scope` er tilgjengelege:
+#' - "none": Utfører ingen transformasjon.
+#' - "spaces": Manipulerar mellomrom mellom teikn på same linje.
+#' - "indention": Manipulerar innrykk, dvs. tal på mellomrom i starten av
+#' kvar linje.
+#' - "line_breaks": Manipulerar linjeskift.
+#' - "tokens": Manipulerar tokens (symbol og teikn).
+#'
+#' `scope` kan spesifiserast på to måtar:
+#' - Som ein tekststreng: I dette tilfellet er alle mindre inngripande
+#' `scope`-nivå underforstått,
+#' t.d. "line_breaks" inkluderar "indention" og "spaces".
+#' Dette er kort og det ein oftast treng.
+#' - Som ein vektor av klassen `AsIs`:
+#' Kvart nivå må listas opp eksplisitt ved å leggja eitt eller fleire nivå
+#' inn i I().
+#'
+#' Sjå 'Examples' for detaljar.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' style_text("call( 1)", style = rapwhale_style, scope = "spaces")
+#' style_text("call( 1)", transformers = rapwhale_style(strict = TRUE))
+#' style_text(c("ab = 3", "a  =3"),
+#'   style = rapwhale_style,
+#'   strict = FALSE
+#' ) # Tek vare på justering av "="
+#' style_text(c("ab = 3", "a  =3"),
+#'   style = rapwhale_style,
+#'   strict = TRUE
+#' ) # Fjernar justering av "="
+#'
+#' # Endrar linjeskift og symbol, men ikkje mellomrom
+#' style_text(c("ab <- 3", "a =3"),
+#'   style = rapwhale_style,
+#'   strict = TRUE, scope = I(c("line_breaks", "tokens"))
+#' )
+rapwhale_style = function(scope = "tokens", strict = TRUE, indent_by = 2,
+                          start_comments_with_one_space = FALSE,
+                          reindention = tidyverse_reindention(),
+                          math_token_spacing = tidyverse_math_token_spacing()) {
+  # Ta utgangspunkt i tidyverse-stilen
+  tidyverse_style(
+    scope = scope, strict = strict, indent_by = indent_by,
+    start_comments_with_one_space = start_comments_with_one_space,
+    reindention = reindention, math_token_spacing = math_token_spacing
+  )
+}
