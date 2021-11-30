f1ad66a99750f81c9c2ba41b7f96d05690b8ac8d
Per Erik Haugedal <per.erik.haugedal@helse-bergen.no>
2021-11-30 14:19:57 +0100

    Modifisert styler-funksjon som legg til krøllparentesar
    I Tidyverse-stilen er det lov å skriva if, else, while, for og
    function på éi linje, men me vil alltid bruka krøllparentesar.
    Har lagt til ein rapwhale-versjon av funksjonen
    styler:::wrap_multiline_curly(), som også legg til krøllparentesar
    for uttrykk på éi linje.

--- fellesting/r-pakkar/rapwhale/R/rapwhale-styler-funksjonar.R
+++ fellesting/r-pakkar/rapwhale/R/rapwhale-styler-funksjonar.R
@@ -1,3 +1,57 @@
+#' Wrap a statement in curly braces
+#'
+#' @description
+#' Wrap a statement in curly braces.
+#'
+#' @param pd
+#' A parse table.
+#' @param indent_by
+#' The amount of spaces used to indent an expression in curly braces.
+#' Used for unindention.
+#' @param space_after
+#' How many spaces should be inserted after the closing brace.
+#' @param key_token
+#' The token that comes right before the token that contains the expression
+#' to be wrapped (ignoring comments).
+#' For if and while loops, this is the closing "')'",
+#' for a for-loop it's "forcond".
+#'
+#' @details
+#' Lik [styler::wrap_multiline_curly],
+#' men der alle uttrykk får krøllparentesar (ikkje berre multi-line).
+wrap_curly_rapwhale = function(pd,
+                               indent_by,
+                               space_after = 1,
+                               key_token) {
+  to_be_wrapped_expr_with_child = styler:::next_non_comment(
+    pd,
+    which(pd$token == key_token)[1]
+  )
+  next_terminal = styler:::next_terminal(
+    pd[to_be_wrapped_expr_with_child, ]
+  )$text
+  requires_braces = if_for_while_part_requires_braces_rapwhale(pd, key_token) &&
+    !any(pd$stylerignore)
+  if (requires_braces | next_terminal == "return") {
+    closing_brace_ind = which(pd$token == key_token)[1]
+    pd$spaces[closing_brace_ind] = 1L
+    all_to_be_wrapped_ind = rlang::seq2(
+      closing_brace_ind + 1L,
+      to_be_wrapped_expr_with_child
+    )
+    pd = styler:::wrap_subexpr_in_curly(
+      pd,
+      all_to_be_wrapped_ind,
+      indent_by,
+      space_after
+    )
+    if (nrow(pd) > 5) {
+      pd$lag_newlines[6] = 0L
+    }
+  }
+  pd
+}
+
 #' Check if if, for or while loop expression require braces
 #'
 #' @description
