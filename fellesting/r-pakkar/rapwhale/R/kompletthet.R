497dafdcfa3b89b8f2e0f6a6421dcf3395c1d9e4
Endre Kvåle Evjen <endre.kvale.evjen@helse-bergen.no>
2021-12-02 12:05:46 +0100

    Lagt inn beskrivelse og skjelett av erstatt_ukjent()

---
+++ fellesting/r-pakkar/rapwhale/R/kompletthet.R
@@ -0,0 +1,14 @@
+
+
+# Erstatt_ukjent
+
+# Funksjonen erstatter utvalgte verdier med NA. Noen datasett
+# (primært MRS-register) har variabler hvor manglende besvarelser får verdi -1,
+# 99 eller andre mer eller mindre vilkårlige verdier for å indikere at spørsmålet
+# ikke er besvart. Disse verdiene finnes i tillegg til *ekte* NA-verdier.
+# Funksjonen tar inn et datasett som inneholder variabelen som skal endres.
+# I tillegg tar den inn en na_vektor med alle verdiene som skal byttes til NA.
+
+erstatt_ukjent = function(data, variabel, na_vektor) {
+
+}
