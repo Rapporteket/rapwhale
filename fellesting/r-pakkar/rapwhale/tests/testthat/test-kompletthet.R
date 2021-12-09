2a6c8442b6b9162a8ae69a843f868b0a80f2390e
Endre Kvåle Evjen <endre.kvale.evjen@helse-bergen.no>
2021-12-09 15:18:08 +0100

    Oppdatert tester for beregn_kompletthet_med_ukjent()

--- fellesting/r-pakkar/rapwhale/tests/testthat/test-kompletthet.R
+++ fellesting/r-pakkar/rapwhale/tests/testthat/test-kompletthet.R
@@ -12,11 +12,13 @@ data_inn = tibble::tibble(
 data_inn_gruppert = data_inn %>%
   group_by(sykehus)
 
+
+
 # Inndata -----------------------------------------------------------------
 test_that("Feilmelding hvis 'data' ikke inneholder nødvendige kolonner", {
   data_uten_var = data_inn %>%
     select(-var_1)
-  feilmelding = "'var_1' mangler i inndata."
+  feilmelding = "'var_1' mangler i inndata"
   expect_error(
     erstatt_ukjent(data_uten_var, variabel = "var_1", na_vektor = -1L),
     feilmelding
@@ -92,6 +94,19 @@ test_that("Fungerer uavhengig av hvilken datatype 'variabel' er", {
 })
 
 test_that("Fungerer med grupperte inndata og ugrupperte inndata", {
+  data_ut_ugruppert = tibble::tibble(
+    pas_id = c(1L:6L),
+    sykehus = c(
+      "HUS", "HUS", "SVG",
+      "SVG", "SVG", "OUS"
+    ),
+    var_1 = c(
+      1L, NA_integer_, NA_integer_,
+      NA_integer_, 2L, NA_integer_
+    ),
+    var_2 = c(1L, 2L, 3L, NA_integer_, -1L, -1L)
+  )
+
   data_ut_gruppert = tibble::tibble(
     pas_id = c(1L:6L),
     sykehus = c(
@@ -106,6 +121,14 @@ test_that("Fungerer med grupperte inndata og ugrupperte inndata", {
   ) %>%
     group_by(sykehus)
 
+
+  expect_identical(
+    erstatt_ukjent(data_inn,
+      variabel = "var_1",
+      na_vektor = c(-1L, 99L)
+    ),
+    data_ut_ugruppert
+  )
   expect_identical(
     erstatt_ukjent(data_inn_gruppert,
       variabel = "var_1",
@@ -114,6 +137,7 @@ test_that("Fungerer med grupperte inndata og ugrupperte inndata", {
     data_ut_gruppert
   )
 })
+
 test_that("Konverterer flere verdier hvis det er oppgitt i na_vektor", {
   data_ut = data_inn %>%
     select(-var_1) %>%
@@ -146,7 +170,7 @@ context("Beregn_kompletthet")
 test_that("Feilmelding hvis 'data' ikke inneholder nødvendige kolonner", {
   data_uten_var = data_inn %>%
     select(-var_1)
-  feilmelding = "'var_1' mangler i inndata."
+  feilmelding = "'var_1' mangler i inndata"
   expect_error(
     beregn_kompletthet(data = data_uten_var, variabel = "var_1"),
     feilmelding
@@ -170,11 +194,11 @@ test_that("Gir riktig resultat for antall og andel missing for ugrupperte data",
 
 test_that("Gir forventet utdata med grupperte data", {
   data_forventet_ut_gruppert = tibble::tibble(
-    sykehus = c("HUS", "SVG", "OUS"),
+    sykehus = c("SVG", "HUS", "OUS"),
     variabelnavn = c(rep("var_1", 3)),
-    totalt_antall = c(2L, 3L, 1L),
+    totalt_antall = c(3L, 2L, 1L),
     antall_na = c(1L, 1L, 0L),
-    andel_na = c(1 / 2, 1 / 3, 0 / 2)
+    andel_na = c(1 / 3, 1 / 2, 0 / 2)
   ) %>%
     group_by(sykehus)
   expect_identical(
@@ -190,10 +214,79 @@ test_that("Gir feilmelding hvis variabel ikke er tekststreng", {
 
 context("Beregn_kompletthet_med_ukjent")
 
-test_that("Feilmelding hvis 'data' ikke inneholder nødvendige kolonner", {})
-test_that("Feilmelding hvis na_vektor er tom", {})
-test_that("Feilmelding hvis na_vektor og variabel har ulik datatype", {})
-test_that("Gir riktig resultat for antall og andel missing", {})
-test_that("Gir forventet utdata med grupperte data", {})
-test_that("Gir feilmelding hvis inndata ikke er tibble/data.frame", {})
-test_that("Gir feilmelding hvis variabel ikke er tekststreng", {})
+test_that("Feilmelding hvis 'data' ikke inneholder nødvendige kolonner", {
+  data_uten_var_1 = data_inn %>%
+    select(-var_1)
+  feilmelding = "'var_1' mangler i inndata"
+
+  expect_error(
+    beregn_kompletthet_med_ukjent(
+      data = data_uten_var_1,
+      variabel = "var_1",
+      na_vektor = c(-1)
+    ),
+    feilmelding
+  )
+})
+test_that("Feilmelding hvis na_vektor er tom", {
+  expect_error(beregn_kompletthet_med_ukjent(
+    data = data_inn,
+    variabel = "var_1"
+  ))
+})
+test_that("Feilmelding hvis na_vektor og variabel har ulik datatype", {
+  expect_error(
+    beregn_kompletthet_med_ukjent(data_inn,
+      variabel = "var_1",
+      na_vektor = "ukjent"
+    ),
+    "'na_vektor' og 'var_1' må være samme datatype"
+  )
+})
+test_that("Gir riktig resultat for antall og andel missing", {
+  data_forventet_ut_med_ukjent_ugruppert = tibble::tibble(
+    variabel = "var_1",
+    antall_totalt = 6L,
+    antall_na = 2L,
+    andel_na = 2 / 6,
+    antall_na_med_ukjent = 4L,
+    andel_na_med_ukjent = 4 / 6
+  )
+
+  expect_identical(
+    beregn_kompletthet_med_ukjent(
+      data = data_inn,
+      variabel = "var_1",
+      na_vektor = c(-1L, 99L)
+    ),
+    data_forventet_ut_med_ukjent_ugruppert
+  )
+})
+
+test_that("Gir forventet utdata med grupperte data", {
+  data_forventet_ut_med_ukjent_gruppert = tibble::tibble(
+    sykehus = c("SVG", "HUS", "OUS"),
+    variabel = c(rep("var_1", 3)),
+    antall_totalt = c(3L, 2L, 1L),
+    antall_na = c(1L, 1L, 0L),
+    andel_na = c(1 / 3, 1 / 2, 0 / 2),
+    antall_na_med_ukjent = c(2L, 1L, 1L),
+    andel_na_med_ukjent = c(2 / 3, 1 / 2, 1 / 1)
+  )
+
+  expect_identical(
+    beregn_kompletthet_med_ukjent(
+      data = data_inn_gruppert,
+      variabel = "var_1",
+      na_vektor = c(-1L, 99L)
+    ),
+    data_forventet_ut_med_ukjent_gruppert
+  )
+})
+
+test_that("Gir feilmelding hvis variabel ikke er tekststreng", {
+  expect_error(beregn_kompletthet_med_ukjent(data_inn,
+    variabel = var_1,
+    na_vektor = c(-1L, 99L)
+  ))
+})
