93e710c1e73208d53d8f6d11d94578b0d8891f08
Endre Kvåle Evjen <endre.kvale.evjen@helse-bergen.no>
2022-02-14 11:45:28 +0100

    Dokumentasjon for datasett-funksjoner

--- fellesting/r-pakkar/rapwhale/R/kompletthet.R
+++ fellesting/r-pakkar/rapwhale/R/kompletthet.R
@@ -151,6 +151,29 @@ beregn_kompletthet_med_ukjent = function(data, variabel, na_vektor) {
 # tibble med oversikt over kompletthet for samtlige variabler. Tar ikke 
 # hensyn til Ukjent/ikke besvart etc. 
 
+#' Beregn kompletthet for datasett
+#'
+#' Funksjonen beregner andel NA verdier for alle variabler i et gitt datasett.
+#' 
+#' @param data Datasett det skal beregnes kompletthet for.
+#'
+#' @return
+#' Returnerer en tibble med oversikt over antall observasjoner, antall NA og 
+#' andel NA for hver variabel. 
+#' @export
+#'
+#' @examples
+#' # Datasett det skal beregnes kompletthet for
+#' d_eksempel = tibble::tibble(
+#' pas_id = c(1L, 2L, 3L, 4L, 5L, 6L), 
+#' sykehus = c("HUS", "HUS", "SUS", "SUS", "SUS", "OUS"), 
+#' vekt = c(60L, NA_integer_, 100L, NA_integer_, 99L, -1L), 
+#' vekt_2 = c(55L, NA_integer_, 99L, -1L, NA_integer_, 50L), 
+#' hoyde = c(1.52, NA_real_, 1.89, 2.15, NA_real_, 99.9), 
+#' symptom = c("svett", "klam", NA_character_, "trøtt", "vet ikke", "Ukjent"),
+#' test_logisk = c(TRUE, FALSE, NA, NA, FALSE, TRUE))
+#' 
+#' beregn_kompletthet_datasett(data = d_eksempel)
 beregn_kompletthet_datasett = function(data) {
   
   variabel = names(data)
@@ -167,18 +190,53 @@ beregn_kompletthet_datasett = function(data) {
   d_na
 }
 
-# beregn_kompletthet_datasett_med_ukjent ----------------------------------
+# Erstatt_ukjent_for_datasett -------------------------------------------------
 
-# Tar inn et datasett det skal beregnes kompletthet for i tillegg til 
-# en tibble/nested-liste med oversikt over hvilke verdier som teller som 
-# ukjent. 
-# Returnerer en tibble med oversikt over direkte kompletthet og indirekte 
-# kompletthet for hele datasettet. 
-
-# FIXME - Lage oppsett for å generere tibble med riktig format for ukjente 
-# verdier for bruk i funksjon. 
-# FIXME - Skrive dokumentasjon for nye funksjoner
-
+#' erstatter ukjente verdier med na for et datasett
+#' 
+#' Enkelte variabler har egne verdier for å indikere manglende besvarelse 
+#' i tillegg til NA-verdier. Eksempelvis har mange variabler i MRS-register
+#' verdien -1 for verditekst "Velg Verdi" eller "Ukjent verdi". 
+#' Funksjonen erstatter disse verdiene med NA for å beregne andel manglende 
+#' besvarelser inkludert disse ukjente verdiene. 
+#' Blir brukt av funksjonen beregn_kompletthet_datasett_med_ukjent(). 
+#'
+#' @param data Tibble/data.frame som inneholder variablene det skal 
+#' erstattes ukjente verdier med NA for. 
+#' @param ukjent_datasett Tibble med variabelnavn og oversikt over hvilke 
+#' verdier som skal erstattes med NA. Se eksempel for hvilken struktur 
+#' ukjent_datasett må ha. 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' d_eksempel = tibble::tibble(
+#'pas_id = c(1L, 2L, 3L, 4L, 5L, 6L), 
+#' sykehus = c("HUS", "HUS", "SUS", "SUS", "SUS", "OUS"), 
+#' vekt = c(60L, NA_integer_, 100L, NA_integer_, 99L, -1L), 
+#' vekt_2 = c(55L, NA_integer_, 99L, -1L, NA_integer_, 50L), 
+#' hoyde = c(1.52, NA_real_, 1.89, 2.15, NA_real_, 99.9), 
+#' symptom = c("svett", "klam", NA_character_, "trøtt", "vet ikke", "Ukjent"),
+#' test_logisk = c(TRUE, FALSE, NA, NA, FALSE, TRUE))
+#' 
+#' ukjent_datasett_eksempel = tibble::tibble(
+#'   variabel = c("pas_id", 
+#'   "sykehus", rep("vekt", 2), 
+#'   rep("vekt_2", 2), 
+#'   "hoyde", rep("symptom", 2), 
+#'   "test_logisk"),
+#'   ukjent_verdi_integer = c(NA_integer_, NA_integer_, 99, 
+#'   -1, 99, -1, NA_integer_, NA_integer_, NA_integer_, NA_integer_),
+#'   ukjent_verdi_real = c(NA_real_, NA_real_, NA_real_, NA_real_, 
+#'   NA_real_, NA_real_, 99.9, NA_real_, NA_real_, NA_real_), 
+#'   ukjent_verdi_tekst = c(NA_character_, NA_character_, 
+#'   NA_character_, NA_character_, NA_character_, NA_character_, 
+#'   NA_character_, "vet ikke", "Ukjent", NA_character_))
+#'   
+#'   erstatt_ukjent_for_datasett(
+#'   data = d_eksempel, 
+#'   ukjent_datasett = ukjent_datasett_eksempel)
 erstatt_ukjent_for_datasett = function(data, ukjent_datasett){
   
   data_uten_ukjent = tibble::tibble_row()
@@ -189,7 +247,6 @@ erstatt_ukjent_for_datasett = function(data, ukjent_datasett){
                                 -variabel) %>% 
                          pull()), otherwise = NULL)
   
-  # 
   for(i in 1:ncol(data)) {
     variabel_i = names(data)[i]
     na_vektor_i = hjelpefunksjon_na_vektor(ukjent_datasett, 
@@ -205,7 +262,53 @@ erstatt_ukjent_for_datasett = function(data, ukjent_datasett){
   data_uten_ukjent
 }
 
+# beregn_kompletthet_datasett_med_ukjent ----------------------------------
 
+#' Beregn kompletthet med og uten ukjente verdier 
+#' 
+#' Funksjonen beregner antall og andel missing med og uten ukjente verdier. 
+#' Først beregnes antall og andel missing NA-verdier direkte. 
+#' I tillegg erstattes ukjente verdier oppgitt i ukjent_datasett med NA 
+#' før antall og andel missing beregnes på nytt. 
+#' Resultat for begge beregninger returneres i utdata. 
+#'
+#' @param data Datasett det skal beregnes kompletthet for. 
+#' @param ukjent_datasett Datasett med oversikt over hvilke verdier og 
+#' variabler som skal erstattes med NA. Se eksempel for detaljer om 
+#' struktur på ukjent_datasett.  
+#'
+#' @return
+#' Tibble med antall observasjoner, antall og andel NA, både med og uten 
+#' erstatning av ukjente verdier for hver variabel i inndata.  
+#' 
+#' @export
+#'
+#' @examples
+#' # Datasett det skal beregnes kompletthet for
+#' d_eksempel = tibble::tibble(
+#' pas_id = c(1L, 2L, 3L, 4L, 5L, 6L), 
+#' sykehus = c("HUS", "HUS", "SUS", "SUS", "SUS", "OUS"), 
+#' vekt = c(60L, NA_integer_, 100L, NA_integer_, 99L, -1L), 
+#' vekt_2 = c(55L, NA_integer_, 99L, -1L, NA_integer_, 50L), 
+#' hoyde = c(1.52, NA_real_, 1.89, 2.15, NA_real_, 99.9), 
+#' symptom = c("svett", "klam", NA_character_, "trøtt", "vet ikke", "Ukjent"),
+#' test_logisk = c(TRUE, FALSE, NA, NA, FALSE, TRUE))
+#'
+#' # Oversikt over hvilke verdier som skal erstattes med NA for hvilke variabler. 
+#' ukjent_datasett = tibble::tibble(
+#'  variabel = c("pas_id", "sykehus", rep("vekt", 2), rep("vekt_2", 2),
+#'  "hoyde", rep("symptom", 2), "test_logisk"),
+#'  ukjent_verdi_integer = c(NA_integer_, NA_integer_, 99, -1, 99, -1,
+#'  NA_integer_, NA_integer_, NA_integer_, NA_integer_),
+#'  ukjent_verdi_real = c(NA_real_, NA_real_, NA_real_, NA_real_, 
+#'  NA_real_, NA_real_, 99.9, NA_real_, NA_real_, NA_real_), 
+#'  ukjent_verdi_tekst = c(NA_character_, NA_character_, NA_character_, 
+#'  NA_character_, NA_character_, NA_character_, NA_character_, 
+#'  "vet ikke", "Ukjent", NA_character_))
+#' 
+#' # Beregner kompletthet
+#' beregn_kompletthet_datasett_med_ukjent(data = d_eksempel, 
+#'                                        ukjent_datasett = ukjent_datasett)
 beregn_kompletthet_datasett_med_ukjent = function(data, ukjent_datasett) {
   
   # Beregner kompletthet for datasett ekskludert ukjente
@@ -226,3 +329,5 @@ beregn_kompletthet_datasett_med_ukjent = function(data, ukjent_datasett) {
   data_ut
   
 }
+
+
