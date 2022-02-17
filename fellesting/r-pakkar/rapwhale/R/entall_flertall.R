a1aa8b5dd6575e494900bb17c11324495606c7e2
Tobias Våge Henriksen <tobias.vage.henriksen@helse-bergen.no>
2022-02-17 15:48:13 +0100

    Lagt til dokumentasjon for funksjon angående rett flertallsbøyning

--- fellesting/r-pakkar/rapwhale/R/entall_flertall.R
+++ fellesting/r-pakkar/rapwhale/R/entall_flertall.R
@@ -1,3 +1,39 @@
+#' Velg riktig form av entall og flertall
+#'
+#' @description
+#' `r lifecycle::badge("experimental")`
+#'
+#' Velger riktig form, entall eller flertall, til å etterfølge et tall.
+#'
+#' @param x Vektor bestående av heltall.
+#' @param entall `character`-verdi som angir ordet i entallsform.
+#' @param flertall `character`-verdi som angir ordet i flertallsform.
+#' @param nulltekst `character`-verdi som angir nullteskt. Standardverdi
+#' er "0".
+#'
+#' @details
+#' Funksjonen velger enten entalls- eller flertallsformen av et ønskelig ord,
+#' hvor begge alternativer (entall og flertall) blir skrevet inn av brukeren.
+#' Ordet etterfølger tallet fra heltallsvektoren som blir lest inn.
+#'
+#' Noen ganger er ikke standardverdien "0" som er tilegnet
+#' `nulltekst`-argumentet et passende ord. Legg da inn det egnede ordet i
+#' dette argumentet.
+#'
+#' @return
+#' Gir ut en `character`-vektor. Denne vektoren består av heltallsvektoren, x,
+#' som ble lest inn, hvor 0 er byttet ut med `"nulltekst"`, etterfulgt av
+#' `"entall"` om heltallet foran er 1, eller `"flertall"` om heltallet foran er
+#' ulik 1.
+#'
+#' @export
+#'
+#' @examples
+#' # Enkeltverdi inndata.
+#' entall_flertall(1, "operasjon", "operasjoner")
+#'
+#' # Vektorverdi inndata, med ikke-standard nulltekst.
+#' entall_flertall(c(0, 1, 2), "nytt", "nye", "ingen")
 entall_flertall = function(x, entall, flertall, nulltekst = "0") {
   if (!is.character(entall) ||
     !is.character(flertall) ||
