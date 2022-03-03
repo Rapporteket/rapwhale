148df5b2bf1e05bcfc8f70c9684322d42357eb04
Karl Ove Hufthammer <karl.ove.hufthammer@helse-bergen.no>
2022-03-03 13:23:25 +0100

    Kvalitetssikra dokumentasjonen til erstatt_0()

--- fellesting/r-pakkar/rapwhale/R/erstatt_0.R
+++ fellesting/r-pakkar/rapwhale/R/erstatt_0.R
@@ -1,25 +1,34 @@
-#' Erstatt tall lik 0 med nulltekst
+#' Erstatt tall lik 0 med valgfri verdi
 #'
 #' @description
 #' `r lifecycle::badge("experimental")`
 #'
-#' Erstatter tall som er lik 0 automatisk med nulltekst.
+#' Erstatter alle tall som er lik 0 med en valgfri verdi.
 #'
-#' @param x Numerisk vektor med minst ett element.
-#' @param nulltekst Tekst som skal erstatte `x`-elementer lik 0.
+#' @param x Numerisk vektor.
+#' @param nullverdi Verdi (tall eller tekst)
+#'   som skal erstatte `x`-elementer lik 0.
 #'
 #' @details
-#' Hver tallverdi i `x`,
-#' med unntak av 0,
-#' blir erstattet av tallverdien i
-#' teksformat.
-#' Verider lik 0 blir erstattet med `nulltekst`.
+#' Alle verdier nøyaktig lik 0
+#' blir erstattet med verdien `nullverdi`.
+#' Funksjonen kan for eksempel brukes til
+#' å erstatte tallet 0 med teksten «ingen» i løpende tekst.
+#'
+#' Funksjonen er hovedsakelig laget for å brukes
+#' sammen med [entall_flertall()].
 #'
 #' @return
-#' Tekstvektor av samme lengde som `x`,
-#' hvor verider lik 0 er erstattet av `nulltekst`.
+#' Vektor av samme lengde og innhold som `x`,
+#' med med 0-verdier byttet ut med `nullverdi`.
+#' Verdiene blir om nødvendig gjort om til
+#' å ha same type som `nullverdi`
+#' (oftest tekst eller tall),
+#' etter standard R-regler.
 #'
 #' @export
 #'
 #' @examples
 #' erstatt_0(c(0, 1, 2), "ingen")
+#' erstatt_0(c(0, 1, 2), -99)
+#' erstatt_0(c(0, 1, 2), NA)
