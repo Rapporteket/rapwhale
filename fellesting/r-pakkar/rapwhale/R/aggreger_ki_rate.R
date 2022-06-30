eec27e321e05e3fabb7b4ded7f38dc4644a9244d
Karl Ove Hufthammer <karl.ove.hufthammer@helse-bergen.no>
2022-06-30 10:06:26 +0200

    Forbetra og meir presis dokumentasjon for aggreger_ki_rate()
    Er òg meir konsistent med dei andre aggregeringsfunksjonane.

--- fellesting/r-pakkar/rapwhale/R/aggreger_ki_rate.R
+++ fellesting/r-pakkar/rapwhale/R/aggreger_ki_rate.R
@@ -3,27 +3,32 @@
 #' @description
 #' `r lifecycle::badge("experimental)`
 #'
-#' Regner ut kvalitetsindikator for rate av numerisk data,
-#' basert på et standard datasett for dette.
-#' Gir også ut konfidensintervall for raten.
+#' Regner ut kvalitetsindikator for rater (antall delt på eksponering),
+#' basert på et standard datasett for ratedata/Poisson-data.
+#' Gir også ut konfidensintervall for ratene.
 #'
 #' @param d_ki_ind Datasett som gitt ut av en standard KI-funksjon for
 #' ratedata.
 #' Se detaljer nedenfor.
 #' @param alfa Én minus nivået til konfidensintervallet.
 #' Standardverdi er 0.05, som tilsvarer et 95 %-konfidensintervall.
-#' @param multiplikator Heltallsverdi som mulitpliseres med raten for å angi
-#' raten per valg av `multiplikator`.
+#' @param multiplikator Tallverdi som skal multipliseres med raten
+#' (for å vise raten per `multiplikator` enheter i utdatene).
 #'
 #' @details
 #' Inndatasettet må inneholde minst de tre variablene `ki_antall`,
 #' `ki_eksponering` og `ki_aktuell`,
 #' der `ki_antall` er en numeriske variabel som angir hvor mange hendelser
-#' som er blitt observert,
-#' `ki_eksponering` er en numerisk variabel som angir eksponeringsforløpet
-#' hvor hendelsene i `ki_antall` er blitt observert,
-#' og `ki_aktuell` er en logisk variabel som indikerer om hendelsen skal
-#' inkluderes i beregningen eller ikke.
+#' som er blitt observert
+#' (for eksempel antall ganger pasienten har fått smertelindring
+#' eller antall observerte bakteriekolonier),
+#' `ki_eksponering` er en numerisk variabel som angir eksponeringen
+#' som var grunnlag for hendelsene i `ki_antall`
+#' (her for eksempel antall pasientdøgn
+#' eller størrelsen på undersøkt område, målt i kvadratcentimeter),
+#' og `ki_aktuell` er en logisk variabel
+#' som indikerer om observasjonen/raden
+#' skal inkluderes i beregningen eller ikke.
 #'
 #' Funksjonen gir ut aggregert datasett med estimert rate og
 #' konfidensintervall for estimatet.
@@ -32,34 +37,29 @@
 #' med én rad per gruppe.
 #'
 #' Om man velger en ikke-standard verdi for `multiplikator`,
-#' for eksempel `1000`,
-#' får man ut estimat av rate og konfidensintervall per `1000`.
-#' Dette kan være hendig om man ser på hendelser per innbygger i en stor
-#' populasjon.
-#'
-#' @note
-#' Funksjonen regner ut estimert rate ved å,
-#' på gruppenivå,
-#' finne det totale antall hendelser,
-#' og dividere med det totale eksponeringsforløpet
-#' (sum(`ki_antall`)/sum(`ki_eksponering`)*`multiplikator`).
-#'
-#' Om man heller er interessert i andelsdata,
-#' bruk [aggreger_ki_prop()].
-#' Ratedata er en måling av frekvensen en hendelse forekommer i et definert
-#' eksponeringsforløp,
-#' mens andelsdata er en sammenligning av en del av dataene med hele datasettet.
-#'
+#' får man ut estimat av rate og tilhørende konfidensintervall
+#' per `multiplikator` av enheten til eksponeringen.
+#' Dette kan være hendig om man for eksempel ser på sjeldne hendelser
+#' per innbygger i en stor populasjon.
+#' Da kan man sette `multiplikator` til `1000` eller `100000`
+#' for å få ut raten per 1000 eller 100 000 innbyggere.
+#' 
+#' @note 
 #' Konfidensintervallene blir regnet ut under forutsetning at dataene er
-#' Poissonfordelt,
-#' og blir da beregnet ved profil-likelihood metoden.
+#' Poisson-fordelt,
+#' og blir da beregnet ved profil-likelihood-metoden.
+#'
+#' @seeother
+#' Om du heller er interessert i andelsdata (teller/nevner),
+#' altså data som følger en binomisk fordeling,
+#' bruk [aggreger_ki_prop()].
 #'
 #' @return
 #' Ugruppert `tibble` eller `data.frame`
 #' (avhengig av inndataene)
 #' med følgende kolonner:
-#' \item{est} Estimert Kvalitetsindikatoren, dvs. estimert rate
-#' (mean(`ki_antall`)/sum(`ki_eksponering`)).
+#' \item{est} Kvalitetsindikatoren, dvs. estimert rate
+#' (sum(`ki_antall`)/sum(`ki_eksponering`)*`multiplikator`).
 #' \item{konfint_nedre} Nedre konfidensgrense for `est`.
 #' \item{konfint_ovre} Øvre konfidensgrense for `est`.
 #'
@@ -72,7 +72,7 @@
 #' # Eksempeldata
 #' helseforetak = c(
 #'   "Helse Vest",
-#'   "Helse Sor Ost",
+#'   "Helse Sør-Øst",
 #'   "Helse Midt",
 #'   "Helse Nord"
 #' )
@@ -100,8 +100,8 @@
 #' # siden grupperingsvariabelen «helseforetak» var en tekstvariabel.
 #' # Hvis du vil ha en annen rekkefølge, gjør den om til faktor først.
 #' d = mutate(d, helsefortak = factor(helseforetak,
-#'   levels = c("Helse Nord", "Helse Midt", "Helse Vest", "Helse Sor Ost")
+#'   levels = c("Helse Nord", "Helse Midt", "Helse Vest", "Helse Sør-Øst")
 #' ))
 #' d %>%
-#'   gruop_by(helseforetak) %>%
+#'   group_by(helseforetak) %>%
 #'   aggreger_ki_rate()
