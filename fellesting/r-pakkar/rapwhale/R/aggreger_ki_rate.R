249f4a651a607b61637ffb2060c472f451b6350b
Tobias Våge Henriksen <tobias.vage.henriksen@helse-bergen.no>
2022-07-08 11:09:50 +0200

    Implementert aggreger_ki_rate()

--- fellesting/r-pakkar/rapwhale/R/aggreger_ki_rate.R
+++ fellesting/r-pakkar/rapwhale/R/aggreger_ki_rate.R
@@ -43,8 +43,8 @@
 #' per innbygger i en stor populasjon.
 #' Da kan man sette `multiplikator` til `1000` eller `100000`
 #' for å få ut raten per 1000 eller 100 000 innbyggere.
-#' 
-#' @note 
+#'
+#' @note
 #' Konfidensintervallene blir regnet ut under forutsetning at dataene er
 #' Poisson-fordelt,
 #' og blir da beregnet ved profil-likelihood-metoden.
@@ -105,3 +105,103 @@
 #' d %>%
 #'   group_by(helseforetak) %>%
 #'   aggreger_ki_rate()
+aggreger_ki_rate = function(d_ki_ind, alfa = 0.05, multiplikator = 1) {
+  # Teste inndata
+  if (!(
+    is.data.frame(d_ki_ind) &&
+      all(hasName(d_ki_ind, c("ki_antall", "ki_eksponering", "ki_aktuell")))
+  )) {
+    stop(paste0(
+      "Inndata må være tibble/data.frame med kolonnene «ki_antall», ",
+      "«ki_eksponering» og «ki_aktuell»"
+    ))
+  }
+  if (!(
+    is.numeric(d_ki_ind$ki_antall) &&
+      is.numeric(d_ki_ind$ki_eksponering) &&
+      is.logical(d_ki_ind$ki_aktuell)
+  )) {
+    stop(paste0(
+      "Kriterievariablen «ki_antall» og «ki_eksponering» må være numerisk, ",
+      "mens «ki_aktuell» må være logisk"
+    ))
+  }
+  if (!all(d_ki_ind$ki_aktuell %in% c(TRUE, FALSE))) {
+    stop("«ki_aktuell» må være TRUE eller FALSE")
+  }
+  if (any(d_ki_ind$ki_aktuell & is.na(d_ki_ind$ki_antall))) {
+    stop("«ki_antall» kan ikke være NA om «ki_aktuell» er TRUE")
+  }
+  if (any(d_ki_ind$ki_aktuell & is.na(d_ki_ind$ki_eksponering))) {
+    stop("«ki_eksponering» kan ikke være NA om «ki_aktuell» er TRUE")
+  }
+  if (!all(
+    (d_ki_ind$ki_aktuell & d_ki_ind$ki_antall >= 0) |
+      (!d_ki_ind$ki_aktuell &
+        (d_ki_ind$ki_antall >= 0) | is.na(d_ki_ind$ki_antall))
+  )) {
+    stop("«ki_antall» kan ikke være mindre enn 0")
+  }
+  if (!all(
+    (d_ki_ind$ki_aktuell & d_ki_ind$ki_eksponering > 0) |
+      (!d_ki_ind$ki_aktuell &
+        (d_ki_ind$ki_eksponering > 0) | is.na(d_ki_ind$ki_eksponering))
+  )) {
+    stop("«ki_eksponering» kan ikke være mindre enn eller lik 0")
+  }
+  if (length(alfa) != 1) {
+    stop("«alfa» må ha lengde 1")
+  }
+  if (length(multiplikator) != 1) {
+    stop("«multiplikator» må ha lengde 1")
+  }
+  if (!is.numeric(alfa) | alfa <= 0 | alfa >= 1) {
+    stop("«alfa» må være et tall mellom 0 og 1")
+  }
+  if (!is.numeric(multiplikator) | multiplikator <= 0 | is.na(multiplikator)) {
+    stop("«multiplikator» må være et positivt tall")
+  }
+  if (any(group_size(d_ki_ind) == 0)) {
+    warning("Det finnes grupper uten observasjoner i grupperingsvariabel")
+  }
+
+  konfint = function(antall, eksponering) {
+    if (length(antall) == 0 | length(eksponering) == 0) {
+      konfint_nedre = NA_real_
+      konfint_ovre = NA_real_
+    } else if (sum(antall) == 0) {
+      konfint_nedre = 0
+      konfint_ovre = -log(alfa) / sum(eksponering)
+    } else {
+      konfint = glm(
+        sum(antall) ~ 1,
+        family = "poisson",
+        offset = log(sum(eksponering))
+      ) %>%
+        profile() %>%
+        confint(level = 1 - alfa) %>%
+        exp()
+      konfint_nedre = konfint[[1]]
+      konfint_ovre = konfint[[2]]
+    }
+    c(konfint_nedre, konfint_ovre)
+  }
+
+  d_ki_ind %>%
+    dplyr::summarise(
+      est = (sum(ki_antall[ki_aktuell], na.rm = TRUE) /
+        sum(ki_eksponering[ki_aktuell], na.rm = TRUE)) %>%
+        replace_na(NA), # Gjør NaN om til NA
+      konfint = list(
+        konfint(ki_antall[ki_aktuell], ki_eksponering[ki_aktuell])
+      ),
+      .groups = "keep"
+    ) %>%
+    dplyr::mutate(
+      est = est * multiplikator,
+      konfint_nedre = map_dbl(konfint, pluck, 1) * multiplikator,
+      konfint_ovre = map_dbl(konfint, pluck, 2) * multiplikator
+    ) %>%
+    dplyr::select(group_cols(d_ki_ind), est, konfint_nedre, konfint_ovre) %>%
+    dplyr::ungroup()
+}
