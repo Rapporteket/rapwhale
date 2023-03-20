37cab8700caa235b9a0fc023f24b73ba21cdb01b
Per Erik Haugedal <per.erik.haugedal@helse-bergen.no>
2023-03-20 10:44:56 +0100

    Køyr test av utviklingsnivaa() med ei eiga testmappe
    Denne mappa har berre éi lita .Rd-fil.

--- tests/testthat/test-utviklingsnivaa.R
+++ tests/testthat/test-utviklingsnivaa.R
@@ -1,6 +1,4 @@
 test_that("utviklingsnivaa() har utviklingsnivå experimental", {
-  nivaa = utviklingsnivaa() %>%
-    filter(funksjon == "utviklingsnivaa()") %>%
-    pluck(2)
+  nivaa = utviklingsnivaa("eksempeldokumentasjon")$utviklingsnivaa
   expect_identical(nivaa, "experimental")
 })
