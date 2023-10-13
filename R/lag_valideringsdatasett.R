a0dec35d562dcd027496b63ee610a77d851a19d4
Sergio Carracedo Huroz <sergio.carracedo.huroz@helse-bergen.no>
2023-10-13 13:23:40 +0200

    Fjernet unødvendige linjeskift

--- R/lag_valideringsdatasett.R
+++ R/lag_valideringsdatasett.R
@@ -73,7 +73,6 @@ lag_valideringsdatasett = function(d_reg, indvars) {
   }
 
   # Lag oversikt over datavariablar
-  
   datavars = setdiff(vars, indvars)
   d_vartypar = tibble::tibble(
     vartypar = map(d_reg[datavars], ~ class(.x)) |>
