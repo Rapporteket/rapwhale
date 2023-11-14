fabfe2efbbd36c13393e50eb73cbb90cc3a936ec
Karl Ove Hufthammer <karl.ove.hufthammer@helse-bergen.no>
2023-11-14 14:02:05 +0100

    Flytta testen

--- tests/testthat/test-utviklingsnivaa.R
+++ tests/testthat/test-utviklingsnivaa.R
@@ -2,3 +2,7 @@ test_that("utviklingsnivaa() har utviklingsnivå experimental", {
   nivaa = utviklingsnivaa("eksempeldokumentasjon")$utviklingsnivaa
   expect_identical(nivaa, "experimental")
 })
+
+test_that("Verda verkar som ho skal", {
+  expect_identical("eple", "appelsinar")
+})
--- tests/testthat/tulletest.R
+++
@@ -1,3 +0,0 @@
-test_that("Verda verkar som ho skal", {
-  expect_identical("eple", "appelsinar")
-})
