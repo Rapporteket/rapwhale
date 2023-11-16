26ba7a0e08c46ce869c2b845d881e14e9914fd4c
Per Erik Haugedal <per.erik.haugedal@helse-bergen.no>
2023-11-16 13:53:06 +0100

    Testar for aktiver_kvalregtema()
    Sjekk at funksjonen:
    - Set tema_kvalreg() som ggplot2-tema
    - Oppdaterer standardverdiar for ggplot2-geom
    - Oppdaterer innstillingar for qicharts2-figurar

--- tests/testthat/test-tema-kvalreg.R
+++ tests/testthat/test-tema-kvalreg.R
@@ -1,3 +1,41 @@
+# Testar for aktiver_kvalregtema
+aktiver_kvalregtema()
+
+test_that("aktiver_kvalregtema() set tema_kvalreg() som ggplot2-tema", {
+  expect_identical(ggplot2::theme_get(), tema_kvalreg())
+})
+
+test_that("aktiver_kvalregtema() set ynskja standardverdiar for ggplot2-geom", {
+  geom_point_standard = ggplot2:::check_subclass("point", "Geom")$default_aes
+  geom_line_standard = ggplot2:::check_subclass("line", "Geom")$default_aes
+  geom_linerange_standard = ggplot2:::check_subclass("linerange",
+    subclass = "Geom"
+  )$default_aes
+  geom_hline_standard = ggplot2:::check_subclass("hline", "Geom")$default_aes
+  geom_vline_standard = ggplot2:::check_subclass("vline", "Geom")$default_aes
+  geom_bar_standard = ggplot2:::check_subclass("bar", "Geom")$default_aes
+  geom_col_standard = ggplot2:::check_subclass("col", "Geom")$default_aes
+
+  expect_identical(geom_point_standard$size, 2)
+  expect_identical(geom_point_standard$colour, "#2171b5")
+  expect_identical(geom_line_standard$linewidth, 1)
+  expect_identical(geom_line_standard$colour, "#2171b5")
+  expect_identical(geom_linerange_standard$linewidth, 0.5)
+  expect_identical(geom_linerange_standard$colour, "#2171b5")
+  expect_identical(geom_hline_standard$linewidth, 0.5)
+  expect_identical(geom_hline_standard$colour, "#000059")
+  expect_identical(geom_vline_standard$linewidth, 0.5)
+  expect_identical(geom_vline_standard$colour, "#000059")
+  expect_identical(geom_bar_standard$fill, "#2171b5")
+  expect_identical(geom_col_standard$fill, "#2171b5")
+})
+
+test_that("aktiver_kvalregtema() set ynskja innstillingar for qicharts2", {
+  expect_identical(getOption("qic.signalcol"), "#FF7260")
+  expect_identical(getOption("qic.linecol"), "#2171b5")
+  expect_identical(getOption("qic.targetcol"), "#c6dbef")
+})
+
 # Testar for tema_kvalreg()
 test_that("Temaet får dei faktisk ynskja verdiane", {
   test_tema = tema_kvalreg()
