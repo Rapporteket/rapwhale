86b0258dc13a273cd4aa955fc4441f7c2c73ec11
Per Erik Haugedal <per.erik.haugedal@helse-bergen.no>
2023-11-16 11:35:50 +0100

    Testar på at tema_kvalreg() gjev ut ynskja verdiar
    For spesifikke innstillingar me har valt sjølv
    (ulikt ggplot2::theme_light()).

--- tests/testthat/test-tema-kvalreg.R
+++ tests/testthat/test-tema-kvalreg.R
@@ -1,3 +1,20 @@
+# Testar for tema_kvalreg()
+test_that("Temaet får dei faktisk ynskja verdiane", {
+  test_tema = tema_kvalreg()
+
+  expect_identical(test_tema$strip.background$fill, "#f3f1ee")
+  expect_identical(test_tema$strip.background$colour, "#e4e0da")
+  expect_identical(test_tema$strip.text.x, element_text(colour = "black"))
+  expect_identical(test_tema$panel.spacing, ggplot2::unit("13", "pt"))
+  expect_identical(test_tema$panel.border$colour, "#e4e0da")
+  expect_identical(test_tema$panel.grid.major$colour, "#e4e0da")
+  expect_identical(test_tema$panel.grid.minor$colour, "#f3f1ee")
+  expect_identical(test_tema$axis.title.y$angle, 0)
+  expect_identical(test_tema$axis.title.y$margin, ggplot2::margin(r = 4.5))
+  expect_identical(test_tema$axis.title.x$margin, ggplot2::margin(t = 4.5))
+  expect_identical(test_tema$plot.margin, ggplot2::margin(3, 3, 3, 3))
+})
+
 # Testing av expand_soyle() og expand_soyle_str_fig()
 
 test_that("expand_soyle() gir ut riktig tallvektor", {
